6cefc7b434033bb851563704e6d6083f1a6c54cc
michael <mskim_451@dongguk.edu>
2021-12-26 23:49:06 +0900

    Update Baek16234.py

--- Python/Baek16234.py
+++ Python/Baek16234.py
@@ -1,80 +1,90 @@
-# 입력
-N, L, R = map(int, input().split())
-graph = []
-groupGraph = []
+import sys
+from collections import deque
 
-groupTotal = [0] * (N * N)
-groupCount = [0] * (N * N)
-visited = []
+def MakeUnionBFS(si, sj):
+  global country, visited, di, dj, L, R, poseCandidate
+  
+  total = 0
+  count = 0
+
+  queue = deque([(si, sj)])
+  union = [(si, sj)]
+  
+  # 방문 처리
+  visited[si][sj] = True
+  count = 1
+  total += country[si][sj]
+
+  while queue:
+    ci, cj = queue.popleft()
+    for i in range(4):
+      # 인접 노드들
+      ni = ci + di[i]
+      nj = cj + dj[i]
+      # 접근 불가하면
+      if not (0 <= ni < len(visited) and 0 <= nj < len(visited[0])):
+        continue
+      # 방문한적 있으면
+      if visited[ni][nj]:
+        continue
+      # 조건 만족하면
+      if L <= abs(country[ni][nj] - country[ci][cj]) <= R:
+        # 다음 후보에 삽입
+        queue.append((ni, nj))
+        visited[ni][nj] = True
+        count += 1
+        total += country[ni][nj]
+        union.append((ni, nj))
+
+  # 평균 값 계산
+  if count > 1:
+    mean = total // count
+    for node in union:
+      country[node[0]][node[1]] = mean
+      # 새로 계산된 곳 근처만 계산하면 된다.
+      poseCandidate.append((node[0], node[1]))
+    return True
+  return False
+
+
+
+  
+
+# 입력
+N, L, R = map(int, sys.stdin.readline().split())
+country = []
 for _ in range(N):
-  graph.append(list(map(int, input().split())))
-visited = [[False] * N for _ in range(N)]
-groupGraph = [[-1] * N for _ in range(N)]
+  country.append(list(map(int, sys.stdin.readline().split())))
+
+# 초기화
+visited = [[False] * N for _ in range(N)] 
+poseCandidate = deque([])
+for i in range(N):
+  for j in range(N):
+    poseCandidate.append((i, j))
+populationPassedDayCount = 0
 
 # 상하좌우
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
 
-def dfs(ci, cj, graph_, visited_, groupIdx_, groupGraph_, groupTotal_, groupCount_):
-  global di, dj, L, R
-  # 방문 처리
-  visited_[ci][cj] = True    
-
-  # 그룹 부여
-  groupGraph_[ci][cj] = groupIdx_
-
-  # 해당 그룹 숫자 + 1
-  groupCount_[groupIdx_] = groupCount_[groupIdx_] + 1
-    
-  # 해당 그룹 총합 계산
-  groupTotal_[groupIdx_] = groupTotal_[groupIdx_] + graph_[ci][cj]
-
-  # 인접 노드 순환
-  for i in range(4):
-    ni = ci + di[i]
-    nj = cj + dj[i]
-    # 접근 불가능한가
-    if ni < 0 or ni > len(graph_) - 1 or nj < 0 or nj > len(graph_[0]) - 1:
-      continue
-    # 방문 했다면
-    if visited_[ni][nj]:
-      continue
-    # 영역 확장 가능하다면
-    if abs(graph_[ni][nj] - graph_[ci][cj]) >= L and abs(graph_[ni][nj] - graph_[ci][cj]) <= R:
-      dfs(ni, nj, graph_, visited_, groupIdx_, groupGraph_, groupTotal_, groupCount_)
-
-
-
-result = 0
-groupIdx = 0
 while True:
-  for i in range(N):
-    for j in range(N):
-        # dfs or bfs make group, calculate mean value
-        if not visited[i][j]:
-          dfs(i, j, graph, visited, groupIdx, groupGraph, groupTotal, groupCount)
-          groupIdx += 1
+  size = len(poseCandidate)
+  unionExist = False
 
-  for i in range(N):
-      for j in range(N):
-        # 특정 그룹에 속하면
-        if groupGraph[i][j] != -1:
-            # 값을 업데이트
-            graph[i][j] = int(groupTotal[groupGraph[i][j]] / groupCount[groupGraph[i][j]])     
+  for _ in range(size):
+    ci, cj = poseCandidate.popleft()
+    if not visited[ci][cj]:
+      if MakeUnionBFS(ci, cj):
+        unionExist = True
 
-  # 그룹 없으면 종료
-  if groupIdx == N * N:
-      break
-  # 초기화
+  if not unionExist:
+    break
+  
   for i in range(N):
     for j in range(N):
       visited[i][j] = False
-      groupTotal[i * N + j] = 0
-      groupCount[i * N + j] = 0
-      groupGraph[i][j] = -1
   
-  result += 1
-  groupIdx = 0
+  populationPassedDayCount += 1
 
-print(result)
-
+print(populationPassedDayCount)
