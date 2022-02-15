e3999025e3a0f1f34f2a56be58c657943dbc49dd
michael <mskim_451@dongguk.edu>
2022-02-15 10:39:08 +0900

    Update FunctionDevelopement.py

--- Python/Programmers/FunctionDevelopement.py
+++ Python/Programmers/FunctionDevelopement.py
@@ -19,3 +19,25 @@ def solution(progresses, speeds):
     
     answer.append(tmp_cnt)
     return answer
+
+
+# ================================================================== #
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    tmp_cnt = 1
+    time_left = list(map(lambda x: math.ceil((100 - progresses[x]) / speeds[x]), range(len(progresses))))
+    
+    for i in range(len(progresses)):
+        try:
+            if time_left[i] < time_left[i + 1]:
+                answer.append(tmp_cnt)
+                tmp_cnt = 1
+            else:
+                time_left[i + 1] = time_left[i]
+                tmp_cnt += 1
+        except IndexError:
+            answer.append(tmp_cnt)
+    
+    return answer
