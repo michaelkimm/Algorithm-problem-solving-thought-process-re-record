bdaf6919df77aad13735b529652bbda24a0032af
michael <mskim_451@dongguk.edu>
2022-02-16 16:18:36 +0900

    Update DiskController.py

--- Python/Programmers/DiskController.py
+++ Python/Programmers/DiskController.py
@@ -34,3 +34,35 @@ def solution(jobs):
             work = heapq.heappop(playable_hp)
             playing.append((work, cur_time + work[0]))
     return answer // job_cnt
+
+
+    # ======================================================== #
+    from collections import deque
+import heapq
+
+def solution(jobs):
+    answer = 0
+    job_q = deque(sorted([[l, s] for s, l in jobs], key=lambda x: (x[1], x[0])))
+    playable_hp = []
+    cur_time = 0
+    
+    # 초기화
+    work = job_q.popleft()
+    heapq.heappush(playable_hp, work)
+    cur_time = work[1]
+    while job_q or playable_hp:
+        # 실행 후 현재시간 업데이트
+        if playable_hp:
+            work = heapq.heappop(playable_hp)
+            cur_time = max(work[1] + work[0], cur_time + work[0])
+            req2end = cur_time - work[1]
+            answer += req2end
+            
+        # 현재 시간 기반하여 job_q에서 playable_hp로
+        while job_q and (job_q[0][1] <= cur_time):
+            heapq.heappush(playable_hp, job_q.popleft()) # 작업 시간, 요청 시간
+        
+        if len(playable_hp) == 0 and job_q:
+            heapq.heappush(playable_hp, job_q.popleft())
+        
+    return answer // len(jobs)
