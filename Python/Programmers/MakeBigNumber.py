eb708563c808edca6a953e97728848ac7e6d07fd
michael <mskim_451@dongguk.edu>
2022-02-20 00:54:49 +0900

    Update MakeBigNumber.py

--- Python/Programmers/MakeBigNumber.py
+++ Python/Programmers/MakeBigNumber.py
@@ -20,3 +20,39 @@ def solution(number, k):
         stack.pop()
     answer = ''.join(stack) + ''.join(list(num_q))
     return answer
+
+# ================================================ #
+
+from collections import deque
+
+def solution(number, k):
+    num_q = deque([ch for ch in number])
+    stack = [num_q.popleft()]
+    # 증가되기 직전 값 제거, 끝까지 없다면 끝값 제거
+    while num_q and k > 0:
+        popped = num_q.popleft()
+        while len(stack) > 0 and stack[-1] < popped and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(popped)
+        
+    for _ in range(k):
+        stack.pop()
+    answer = ''.join(stack) + ''.join(list(num_q))
+    return answer
+
+# ================================================= #
+from collections import deque
+
+def solution(number, k):
+    stack = [number[0]]
+    # 증가되기 직전 값 제거, 끝까지 없다면 끝값 제거
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+    
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
