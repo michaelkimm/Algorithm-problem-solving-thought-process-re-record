f1aca3e7b9205ece427c2e341c90960b4fcd7c61
michael <mskim_451@dongguk.edu>
2022-02-21 09:30:33 +0900

    Create IntegerTriangle.py

---
+++ Python/Programmers/IntegerTriangle.py
@@ -0,0 +1,13 @@
+def solution(triangle):
+
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == (len(triangle[i]) - 1):
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    answer = max(triangle[-1])
+    return answer
