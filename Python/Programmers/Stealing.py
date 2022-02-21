4367d73a42af2fa10f7f3837621919e23f2f0645
michael <mskim_451@dongguk.edu>
2022-02-21 11:40:49 +0900

    Update Stealing.py

--- Python/Programmers/Stealing.py
+++ Python/Programmers/Stealing.py
@@ -46,7 +46,5 @@ def solution(money):
         dp1[i] = [max(dp1[i - 1][0], dp1[i - 1][1]), dp1[i - 1][0] + money[i]]
         dp2[i] = [max(dp2[i - 1][0], dp2[i - 1][1]), dp2[i - 1][0] + money[i]]
     
-    dp1[len(money) - 1] = dp1[len(money) - 2]
-    
-    answer = max(max(dp1[-1]), max(dp2[-1]))
+    answer = max(max(dp1[-2]), max(dp2[-1]))
     return answer
