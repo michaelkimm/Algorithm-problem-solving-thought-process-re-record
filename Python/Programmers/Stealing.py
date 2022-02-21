a0ac56e00f9c6afe97b0d788ee8ccbef6006d52c
michael <mskim_451@dongguk.edu>
2022-02-21 11:33:56 +0900

    Update Stealing.py

--- Python/Programmers/Stealing.py
+++ Python/Programmers/Stealing.py
@@ -27,3 +27,26 @@ def solution(money):
     
     answer = max(first_picked, first_not_picked)
     return answer
+
+
+# =============================================================== #
+def solution(money):
+    # 첫째 집을 뽑은 경우
+    # 첫번째 = 나를 안뽑은 경우, 두번째 = 나를 뽑은 경우
+    dp1 = [[0, 0] for _ in range(len(money))]
+    dp1[0] = [money[0], money[0]]
+    dp1[1] = [money[0], money[0]]
+    
+    # 첫째 집을 안뽑은 경우
+    # 첫번째 = 나를 안뽑은 경우, 두번쨰 = 나를 뽑은 경우
+    dp2 = [[0, 0] for _ in range(len(money))]
+    dp2[1] = [0, money[1]]
+    
+    for i in range(2, len(money)):
+        dp1[i] = [max(dp1[i - 1][0], dp1[i - 1][1]), dp1[i - 1][0] + money[i]]
+        dp2[i] = [max(dp2[i - 1][0], dp2[i - 1][1]), dp2[i - 1][0] + money[i]]
+    
+    dp1[len(money) - 1] = dp1[len(money) - 2]
+    
+    answer = max(max(dp1[-1]), max(dp2[-1]))
+    return answer
