4eb4bc8f28a076e650f4c39345216ec5852102b4
michael <mskim_451@dongguk.edu>
2022-03-05 22:01:18 +0900

    Update MatrixBorderRotate.py

--- Python/Programmers/MatrixBorderRotate.py
+++ Python/Programmers/MatrixBorderRotate.py
@@ -1,9 +1,5 @@
 def my_rotate(ary, i0, j0, i1, j1):
     min_val = int(10002)
-    i0 -= 1
-    j0 -= 1
-    i1 -= 1
-    j1 -= 1
     start_val = ary[i0][j0]
     # 왼쪽
     for i in range(i0, i1):
@@ -28,9 +24,8 @@ def my_rotate(ary, i0, j0, i1, j1):
 
 def solution(rows, columns, queries):
     ary = [[j + columns * i for j in range(1, columns + 1)] for i in range(rows)]
-
     answer = []
-    for query in queries:
-        ary, min_val = my_rotate(ary, query[0], query[1], query[2], query[3])
+    for i0, j0, i1, j1 in queries:
+        ary, min_val = my_rotate(ary, i0 - 1, j0 - 1, i1 - 1, j1 - 1)
         answer.append(min_val)        
     return answer
