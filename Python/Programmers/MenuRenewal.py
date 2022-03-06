fb4b7e8f9df2ce54d14d1d3f55f2fb58825e4c4a
michael <mskim_451@dongguk.edu>
2022-03-06 13:16:37 +0900

    Update MenuRenewal.py

--- Python/Programmers/MenuRenewal.py
+++ Python/Programmers/MenuRenewal.py
@@ -5,9 +5,9 @@ def solution(orders, course):
     # 각 메뉴 콤비 카운트
     menu_combi = defaultdict(int)
     for order in orders:
-        tmp_order = sorted([ch for ch in order])    # (X,Y), (Y,X)는 (X,Y)로 통일 by sort
-        for cnt in range(2, len(order) + 1):
-            tmp_combi = list(combinations(tmp_order, cnt))
+        for course_size in course:
+            # (X,Y), (Y,X)는 (X,Y)로 통일 by sort
+            tmp_combi = list(combinations(sorted(order), course_size))
             for combi in tmp_combi:
                 menu_combi[combi] += 1
                 
@@ -31,3 +31,22 @@ def solution(orders, course):
     
     result.sort()
     return result
+
+# ====================================================== #
+from collections import defaultdict
+from collections import Counter
+from itertools import combinations
+
+def solution(orders, course):
+    result = []
+    for course_size in course:
+        combi_list = []
+        for order in orders:
+            # (X,Y), (Y,X)는 (X,Y)로 통일 by sort
+            combi_list += combinations(sorted(order), course_size)
+            
+        most_common_list = Counter(combi_list).most_common()
+        # 1번 이상 나타남 and 최대 빈도 수와 같은 경우 추가
+        result += [k for k, v in most_common_list if v > 1 and v == most_common_list[0][1]]
+    
+    return sorted([''.join(k) for k in result])
