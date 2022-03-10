220662bf98fc0c6fdfa8f4324172b31f9b3aa451
michael <mskim_451@dongguk.edu>
2022-03-10 10:40:08 +0900

    Update AnticipatedMatch.py

--- Python/Programmers/AnticipatedMatch.py
+++ Python/Programmers/AnticipatedMatch.py
@@ -22,3 +22,13 @@ def solution(n,a,b):
         answer -= 1
         
     return answer
+
+# ============================================== #
+
+def solution(n,a,b):
+    answer = 0
+    while a != b:
+        answer += 1
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+    return answer
