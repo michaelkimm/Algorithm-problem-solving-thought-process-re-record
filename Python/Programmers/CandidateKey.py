ea8654b001f57f201cc678e5c4a2c7a19fd86db7
michael <mskim_451@dongguk.edu>
2022-03-11 14:17:57 +0900

    Update CandidateKey.py

--- Python/Programmers/CandidateKey.py
+++ Python/Programmers/CandidateKey.py
@@ -29,3 +29,40 @@ def solution(relation):
             answer += 1
         
     return answer
+
+
+
+
+# ============================================== #
+
+import itertools
+
+def solution(relation):
+    # 8C1 ~ 8C8 = 250
+    # 각 키당 20번씩 검색
+    key_check_list = []
+    for i in range(1, len(relation[0]) + 1):
+        key_check_list.extend(set(k for k in tuple_k) for tuple_k in itertools.combinations(range(len(relation[0])), i))
+            
+    candidate_keys = []
+    answer = 0
+    for key in key_check_list:
+        available = True
+        # 최소성 확인
+        for ck in candidate_keys:
+            if ck.issubset(key):
+                available = False
+                break
+        if not available:
+            continue
+            
+        # 유일성 확인
+        tmp_set = set()
+        for data in relation:
+            data_piece = tuple(data[k] for k in key)
+            tmp_set.add(data_piece)
+        if len(tmp_set) == len(relation):
+            candidate_keys.append(key)
+            answer += 1
+        
+    return answer
