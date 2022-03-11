b2c06574a2db837c3221bd40d97de545e21f7742
michael <mskim_451@dongguk.edu>
2022-03-11 14:04:33 +0900

    Create CandidateKey.py

---
+++ Python/Programmers/CandidateKey.py
@@ -0,0 +1,31 @@
+import itertools
+def solution(relation):
+    # 8C1 ~ 8C8 = 250
+    # 각 키당 20번씩 검색
+    key_check_list = []
+    for i in range(1, len(relation[0]) + 1):
+        for tuple_k in itertools.combinations(range(len(relation[0])), i):
+            key_check_list.append(set(k for k in tuple_k))
+            
+    candidate_keys = []
+    answer = 0
+    for key in key_check_list:
+        available = True
+        # 최소성 확인
+        for ck in candidate_keys:
+            if key.intersection(ck) == ck:
+                available = False
+                break
+        if not available:
+            continue
+            
+        # 유일성 확인
+        tmp_set = set()
+        for data in relation:
+            data_piece = tuple(data[k] for k in key)
+            tmp_set.add(data_piece)
+        if len(tmp_set) == len(relation):
+            candidate_keys.append(key)
+            answer += 1
+        
+    return answer
