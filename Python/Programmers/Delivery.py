b36b2bc0a59087d168117a832abcf03cde2e0ab3
michael <mskim_451@dongguk.edu>
2022-03-21 21:08:51 +0900

    Update Delivery.py
    알고리즘: 다익스트라
    자료구조: 그래프

--- Python/Programmers/Delivery.py
+++ Python/Programmers/Delivery.py
@@ -20,3 +20,36 @@ def solution(N, road, K):
             answer += 1
 
     return answer
+
+# =============================================================== #
+
+import heapq
+
+def solution(N, road, K):
+    answer = 0
+    INF = int(1e10)
+    distance = [INF] * (N + 1)
+    start = 1
+    graph = [[] for i in range(N + 1)]
+    for a,b,c in road:
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        # 최단 거리가 가장 짧은 노드 정보 출력
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+                
+    for i in range(1, N + 1):
+        if distance[i] <= K:
+            answer += 1
+
+    return answer
