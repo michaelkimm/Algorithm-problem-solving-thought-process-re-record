2ae794ad266397f20fe2c6ca30a2dc920fd0213d
michael <mskim_451@dongguk.edu>
2022-03-21 21:38:10 +0900

    Update 2xNTiling.py
    알고리즘: 피보나치 수열
    자료구조: 배열

--- Python/Programmers/2xNTiling.py
+++ Python/Programmers/2xNTiling.py
@@ -7,3 +7,17 @@ def solution(n):
         cur = bf + bbf
         bbf, bf = bf, cur
     return cur % 1000000007
+
+
+# ================================= #
+
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    if n == 1:
+        return 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007
+    answer = dp[n]
+    return answer
