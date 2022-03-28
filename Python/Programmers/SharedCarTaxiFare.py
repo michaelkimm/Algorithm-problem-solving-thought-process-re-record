4bcde4cb8e744cb5755599d0ca8f4ac2747504b8
michael <mskim_451@dongguk.edu>
2022-03-28 19:54:21 +0900

    Update SharedCarTaxiFare.py
    알고리즘: 다익스트라
    자료구조: 힙 큐
    사고방식: 시작 노드 -> 합승 완료 지점 + 합승 완료 지점 -> a + 합승 완료 지점 -> b 최솟값

--- Python/Programmers/SharedCarTaxiFare.py
+++ Python/Programmers/SharedCarTaxiFare.py
@@ -18,3 +18,37 @@ def solution(n, s, a, b, fares):
         
     answer = min(distances[1:])
     return answer
+
+# =============================================================== #
+
+import heapq
+
+def dijkstra(start, end, graph):
+    distance = 0
+    return distance
+
+def solution(n, s, a, b, fares):
+    INF = int(1e10)
+    distance = [0] * (n + 1)
+    graph = [[] for _ in range(n + 1)]
+    for u, v, c in fares:
+        graph[u].append((v, c))
+        graph[v].append((u, c))
+    # s -> 모든 노드
+    for node in range(1, n + 1):
+        visited = [False] * (n + 1)
+        dist = [INF] * (n + 1)
+        dist[node] = 0
+        hp = [(0, node)]
+        while hp:
+            cost, cur_node = heapq.heappop(hp)
+            if cost > dist[cur_node]:
+                continue
+            for next_node, nc in graph[cur_node]:
+                new_cost = cost + nc
+                if new_cost < dist[next_node]:
+                    dist[next_node] = new_cost
+                    heapq.heappush(hp, (new_cost, next_node))
+        distance[node] = dist[s] + dist[a] + dist[b]
+    answer = min(distance[1:])
+    return answer
