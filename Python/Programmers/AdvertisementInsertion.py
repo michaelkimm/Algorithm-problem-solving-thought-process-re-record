6d1a295eca1f4a14447c05c1dee57ad92bbb7a81
michael <mskim_451@dongguk.edu>
2022-04-04 23:32:13 +0900

    Create AdvertisementInsertion.py
    알고리즘: 이진탐색
    자료구조: 배열
    사고 과정
    - 후보 광고 재생 시간과 겹칠 수 있는 플레이 기록을 찾기 위해 이진 탐색을 사용함. 플레이 기록이 최대 30만회가 될 수 있기 때문에 매회 30만 회를 검색하면 안되기 때문. 이진탐색을 위해 배열을 미리 정렬함.
    
    한계
    - 후보 광고 플레이 시간과 겹치는 플레이 기록들을 검색하는 시간이 최대 30만이 될 수가 있음. O(1)로 줄여야함.

---
+++ Python/Programmers/AdvertisementInsertion.py
@@ -0,0 +1,96 @@
+from bisect import bisect_left, bisect_right
+
+class KeyifyList(object):
+    def __init__(self, inner, key):
+        self.inner = inner
+        self.key = key
+
+    def __len__(self):
+        return len(self.inner)
+
+    def __getitem__(self, k):
+        return self.key(self.inner[k])
+
+
+def get_total_adv_played(logs, adv_start, adv_end):
+    adv_seen_time = 0
+    for log in logs:
+        s_log, e_log = log[0], log[1]
+        if  e_log >= adv_end:
+            if s_log <= adv_start:
+                adv_seen_time += (adv_end - adv_start)
+            else:
+                adv_seen_time += (adv_end - e_log)
+        else:
+            if adv_start <= s_log:
+                adv_seen_time += (e_log - s_log)
+            else:
+                adv_seen_time += (e_log - adv_start)
+    return adv_seen_time
+
+def get_sec_from_log(log):
+    tmp_log = log.split(':')
+    sec =  int(tmp_log[0]) * 3600 + int(tmp_log[1]) * 60 + int(tmp_log[2])
+    return sec
+def get_log_from_sec(sec):
+    answer_h = sec // 3600
+    answer_m = (sec % 3600) // 60
+    answer_s = (sec % 3600) % 60
+    return str(answer_h).zfill(2) + ':' + str(answer_m).zfill(2) + ':' + str(answer_s).zfill(2)
+
+def solution(play_time, adv_time, logs):
+    sec_logs = []
+    play_time_sec = get_sec_from_log(play_time)
+    adv_time_sec = get_sec_from_log(adv_time)
+    for log in logs:
+        start_log, end_log = log.split('-')
+        start_time = get_sec_from_log(start_log)
+        end_time = get_sec_from_log(end_log)
+        sec_logs.append((start_time, end_time))
+        
+    end_sorted_logs = sorted(sec_logs, key=lambda x:x[1])
+    start_sorted_logs = sorted(sec_logs)
+    answer_start, answer_played = 0, 0
+    # 시작 시간 정렬
+    for s_sec, e_sec in start_sorted_logs:
+        target_start, target_end = s_sec, s_sec + adv_time_sec
+        # 경계 처리
+        if target_end > play_time_sec:
+            continue
+        print("te:", target_end, "pts:", play_time_sec)
+        target_logs = []
+        # 타겟 시작 시간 보다 빨리 끝나는 로그 제외
+        left_idx = bisect_left(KeyifyList(end_sorted_logs, key=lambda x: x[1]), target_start)
+        # 타겟 끝 시간 보다 늦게 시작하는 로그 제외
+        right_idx = bisect_right(KeyifyList(start_sorted_logs, key=lambda x: x[0]), target_end)
+        if left_idx > right_idx:
+            continue
+        total_adv_played_time = get_total_adv_played(start_sorted_logs[left_idx:right_idx], target_start, target_end)
+        
+        if answer_played < total_adv_played_time:
+            print("ap:", answer_played, "tapt:", total_adv_played_time)
+            answer_played = total_adv_played_time
+            answer_start = target_start
+    print("==")
+    # 끝시간 맞춤
+    for s_sec, e_sec in end_sorted_logs:
+        target_start, target_end = e_sec - adv_time_sec, e_sec
+        # 경계 처리
+        if target_start < 0:
+            continue
+        print(get_log_from_sec(target_start))
+        target_logs = []
+        # 타겟 시작 시간 보다 빨리 끝나는 로그 제외
+        left_idx = bisect_left(KeyifyList(end_sorted_logs, key=lambda x: x[1]), target_start)
+        # 타겟 끝 시간 보다 늦게 시작하는 로그 제외
+        right_idx = bisect_right(KeyifyList(start_sorted_logs, key=lambda x: x[0]), target_end)
+        if left_idx > right_idx:
+            continue
+        total_adv_played_time = get_total_adv_played(start_sorted_logs[left_idx:right_idx], target_start, target_end)
+        
+        if answer_played < total_adv_played_time:
+            print("ap:", answer_played, "tapt:", total_adv_played_time)
+            answer_played = total_adv_played_time
+            answer_start = target_start
+    
+    return get_log_from_sec(answer_start)
