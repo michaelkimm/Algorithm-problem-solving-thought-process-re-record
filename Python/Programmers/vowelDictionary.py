2e34f255dbcfd608b9bafb705cfc5783d72566fd
michael <mskim_451@dongguk.edu>
2022-04-05 21:31:17 +0900

    Update vowelDictionary.py
    알고리즘: 중복 순열, 이진 탐색(+)
    자료구조: 리스트
    사고 과정
    - vowerDictonary.py 참조
    - 탐색 과정을 이진 탐색으로 하면 더 빠를 것으로 예상. but 탐색 범위 자체가 작기 때문에 역시 효과는 미미했음.

--- Python/Programmers/vowelDictionary.py
+++ Python/Programmers/vowelDictionary.py
@@ -7,3 +7,17 @@ def solution(word):
         words += tmp_words
     words.sort()
     return words.index(word) + 1
+
+
+# ============================================ #
+
+import itertools
+from bisect import bisect_left
+def solution(word):
+    words = []
+    alphabets = ['A', 'E', 'I', 'O', 'U']
+    for i in range(1, len(alphabets) + 1):
+        tmp_words = [''.join(chars) for chars in list(itertools.product(alphabets, repeat = i))]
+        words += tmp_words
+    words.sort()
+    return bisect_left(words, word) + 1
