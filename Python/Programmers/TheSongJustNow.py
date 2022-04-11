860b32eb93b90c6c997e7df1438bb402e29aa7b5
michael <mskim_451@dongguk.edu>
2022-04-11 15:24:54 +0900

    Create TheSongJustNow.py
    알고리즘: 동적 계획법
    
    자료구조: 배열
    
    사고 과정
    1. 내가 들은 멜로디와 곡의 가사가 같은지 확인하기 위해서 가장 긴 공통 문자열을 찾아야한다. 2차원 dp로 이를 찾을 수 있다.
    2. 라디오가 덜 재생되거나 반복 재생 될 수 있기 때문에 가사를 그만큼 자르거나 이어 붙여줘야 하는 사전 작업이 필요하다.

---
+++ Python/Programmers/TheSongJustNow.py
@@ -0,0 +1,40 @@
+import re
+def get_sequential_intesect_cnt(str1, str2):
+    dp = [[0] * len(str2) for _ in range(len(str1))]
+    max_cnt = 0
+    for idx1 in range(len(str1)):
+        for idx2 in range(len(str2)):
+            val = 1 if str1[idx1] == str2[idx2] else 0
+            dp[idx1][idx2] = dp[idx1 - 1][idx2 - 1] + val if idx1 - 1 >= 0 and idx2 - 1 >= 0 else val
+            max_cnt = max(max_cnt, dp[idx1][idx2])
+    return max_cnt
+    
+def time_str_to_min(time):
+    hr, m = time.split(':')
+    return int(hr) * 60 + int(m)
+
+def str_notes_to_list(str_note):
+    # A, A#, B, C, C#, D, D#, E, F, F#, G, G#
+    result = re.findall('[A-Z]#?', str_note)
+    return result
+    
+def solution(m, musicinfos):
+    m = str_notes_to_list(m)
+    candidates = []
+    for idx, music_info in enumerate(musicinfos):
+        start_time, end_time, title, content = music_info.split(',')
+        start_time = time_str_to_min(start_time)
+        end_time = time_str_to_min(end_time)
+        content = str_notes_to_list(content)
+        content_unit = content
+        while (end_time - start_time) > len(content):
+            content += content_unit
+        content = content[:end_time - start_time]   
+        common_cnt = get_sequential_intesect_cnt(m, content)
+        if common_cnt == len(m):
+            candidates.append((end_time - start_time, idx, title))
+    answer = "(None)"
+    if candidates:
+        candidates.sort(key=lambda x:(-x[0], x[1]))
+        answer = candidates[0][2]
+    return answer
