5e177ce7c01bdc3bc84a89e33e9036acb7f6b0c5
michael <mskim_451@dongguk.edu>
2022-04-12 12:13:41 +0900

    Create MatchingTheCard.py
    알고리즘: 브루트포스, 재귀, bfs
    자료구조: 큐
    사고 과정
    1. 처음엔 그리디라고 판단. 가장 가까운 것부터 뒤집어나가면 좋지 않을까 생각했기 때문. 하지만 앞에의 선택이 뒤의 선택에 영향을 미치기 때문에 그리디가 아니었음. 경우의 수가 너무 많고 규칙을 찾을 수 없기 때문에 브루트포스를 생각.
    2. 브루트포스가 될 수 있는지 확인하기 위해 최악의 경우의수 계산. 브루트포스로 계산된 방문 순서 갯수는 1천만개임. 방문 시 최단 경로 계산은 최악의 경우 24log(16)=96임. 즉 전체 최악의 경우는 9억6천만번 연산임. 너무 많다고 볼 수 있지만 최단 경로 최악은 일어나기 쉽지 않음. 건너뛰기 움직임이 있기 때문. 따라서 시간 제한 맞출 수 있을 것 같다고 판단.
    3. 각 브루트포스 경우에 대해 개별 연산이 이뤄져야함. 따라서 매 부르트 포스 함수마다 새로운 board와 남은 뒤집을 카드 리스트를 주기 위해 재귀 함수를 활용함. 재귀 함수로부터 얻는 값은 키 조작 횟수임. 즉 각 브루트 포스 경우에 대해 키 조작 횟수를 반환 받는 것임.

---
+++ Python/Programmers/MatchingTheCard.py
@@ -0,0 +1,95 @@
+from collections import deque
+def bfs(board, si, sj, target_i, target_j):
+    # 동서남북
+    di = [0, 0, 1, -1]
+    dj = [1, -1, 0, 0]
+    q = deque([(0, si, sj)])
+    visited = [[False] * 4 for _ in range(4)]
+    visited[si][sj] = True
+    cost = int(1e10)
+    while q:
+        cost, ci, cj = q.popleft()
+        if ci == target_i and cj == target_j:
+            break
+        # 4방향 한칸
+        for idx in range(4):
+            ni = ci + di[idx]
+            nj = cj + dj[idx]
+            if 0 <= ni <= 3 and 0 <= nj <= 3 and not visited[ni][nj]:
+                q.append((cost + 1, ni, nj))
+                visited[ni][nj] = True
+        # 4방향 건너뛰기
+        for idx in range(4):
+            ni, nj = ci, cj
+            # 동
+            if idx == 0:
+                for j in range(cj + 1, 4):
+                    if board[ni][j] != 0:
+                        nj = j
+                        break
+                    nj = 3
+            # 서
+            elif idx == 1:
+                for j in range(cj - 1, -1, -1):
+                    if board[ni][j] != 0:
+                        nj = j
+                        break
+                    nj = 0
+            # 남
+            elif idx == 2:
+                for i in range(ci + 1, 4):
+                    if board[i][nj] != 0:
+                        ni = i
+                        break
+                    ni = 3
+            # 북
+            elif idx == 3:
+                for i in range(ci - 1, -1, -1):
+                    if board[i][nj] != 0:
+                        ni = i
+                        break
+                    ni = 0
+            if not visited[ni][nj]:
+                q.append((cost + 1, ni, nj))
+                visited[ni][nj] = True
+    return cost
+        
+def brute_force(board, card_list, si, sj):
+    if len(card_list) == 0:
+        return 0
+    
+    results = [0] * len(card_list)
+    for idx, card in enumerate(card_list):
+        c_num, ci, cj = card
+        new_board = [[board[i][j] for j in range(4)] for i in range(4)]
+        # si, sj에서 card까지 가기, 뒤집기
+        key_control_cnt = bfs(new_board, si, sj, ci, cj)
+        key_control_cnt += 1
+        new_board[ci][cj] = 0
+        
+        # card 짝 까지 가기, 뒤집기
+        m_c_i, m_c_j = 0, 0
+        for c in card_list:
+            if c[0] == c_num and not (c[1] == ci and c[2] == cj):
+                m_c_i, m_c_j = c[1], c[2]
+        key_control_cnt += bfs(new_board, ci, cj, m_c_i, m_c_j)
+        key_control_cnt += 1
+        new_board[m_c_i][m_c_j] = 0
+        results[idx] += key_control_cnt
+        
+        # 다음 경우의 수 구하기
+        new_card_list = [c for c in card_list if c[0] != c_num]
+        results[idx] += brute_force(new_board, new_card_list, m_c_i, m_c_j)
+    return min(results)
+    
+def solution(board, r, c):
+    # 뒤집을 카드 위치
+    card_list = []
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] != 0:
+                card_list.append((board[i][j], i, j))
+    
+    new_board = [[board[i][j] for j in range(4)] for i in range(4)]
+    answer = brute_force(new_board, card_list, r, c)
+    return answer
