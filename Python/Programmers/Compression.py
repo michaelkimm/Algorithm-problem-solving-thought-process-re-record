eb061adb7ccf031a5ef1ad8777ec676403dbe141
michael <mskim_451@dongguk.edu>
2022-04-17 17:47:22 +0900

    Update Compression.py
    알고리즘: 구현
    자료구조: 딕셔너리
    사고 과정
    1. 구현 문제임. 문제에서 요구하는 사항을 알맞은 자료구조를 사용하여 구현하면 됨. 이전에는 입력-결과만 맞는 방식으로 논리를 구현했지만, 이번에는 문제에서 요구하는 대로 작성함.
    2.  특정 문자열과 값이 1대1 대응해야 하므로 딕서너리를 사용함.
    3. 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾기 위해선 우측 범위만 알면 된다. 따라서 우측 포인터만 선언하여 현재 탐색 범위 설정.

--- Python/Programmers/Compression.py
+++ Python/Programmers/Compression.py
@@ -25,3 +25,45 @@ def solution(msg):
       
   answer = compressed
   return answer
+
+
+  # ====================================================  #
+
+  def solution(msg):
+    word_dict = { chr(64 + idx) for idx in range(1, 27)}
+    
+    answer = []
+    left, right = 0, 0
+    while left < len(msg):
+        right = left
+        while right < len(msg) and msg[left:right + 1] in word_dict:
+            right += 1
+        if right < len(msg):
+            word_dict[msg[left:right + 1]] = len(word_dict) + 1
+        answer.append(word_dict[msg[left:right]])
+        left = right
+    
+    return answer
+
+
+
+  # ============================================== #
+
+  def solution(msg):
+    word_dict = {chr(64 + idx):idx for idx in range(1, 27)}
+    
+    answer = []
+    while msg:
+        right = 0
+        w = ''
+        while right < len(msg) and msg[:right + 1] in word_dict:
+            right += 1
+        right -= 1
+        w = msg[:right + 1]
+        
+        answer.append(word_dict[w])
+        msg = msg[right + 1:]
+        if msg:
+            word_dict[w + msg[0]] = len(word_dict) + 1
+    
+    return answer
