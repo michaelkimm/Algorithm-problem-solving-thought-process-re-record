2942fa796d9c7c814d50d238f9b58e30dc89967d
michael <mskim_451@dongguk.edu>
2022-04-19 20:16:29 +0900

    Update BlockMoving.py
    알고리즘: bfs
    자료구조: 큐
    사고 과정
    1. 평행 이동과 회전을 할 수 있는 로봇을 목적지까지 최단 시간 안에 가게 하면 된다. 최단 시간은 최단 거리로 해석될 수 있으므로 bfs로 구현해보자.
    2. 노드와 간선을 정의해보자. 간선은 모든 이동 경우의 수다. 가로와 세로 모양을 고려하면 8가지 회전이 있고 8가지 이동 방법을 찾을 수 있다. 즉 16가지의 간선이 존재한다.
    3. 노드는 현재 로봇의 위치다. 노드 위치는 두개의 좌표로 나타내거나 한개의 좌표와 방향으로 나타낼 수 있다. 후자를 택하면 고려할게 많다고 판단되어 전자로 노드를 표현하기로 결정했다. 이때 전자도 회전 행위를 할 때 회전 중심 논쟁이 존재할 수 있다. 따라서 i,j 위치 순서대로 로봇 노드 위치를 저장했다. 그 결과 회전 중심을 뭘로할지 명시적으로 작성할 수 있었다.
    
    총평
    - 해당 문제는 bfs를 이용해서 풀어야한다고 금방 알아챘지만, 노드를 정의하고 간선 처리하는데 까다로웠다. 꼼꼼한 구현이 중요했다.

--- Python/Programmers/BlockMoving.py
+++ Python/Programmers/BlockMoving.py
@@ -1,73 +1,96 @@
 from collections import deque
-def check_movable(pos_set, movement, board):
-    next_pos_set = set()
-    c1, c2 = list(pos_set)
+
+def check_movable(pos, movement, board):
+    next_pos = tuple()
+    c1, c2 = pos
     result = False
+    is_bottom_zero = (board[c1[0] + 1][c1[1]] == 0 and board[c2[0] + 1][c2[1]] == 0) if c1[0] + 1 < len(board) and c2[0] + 1 < len(board) else False
+    is_top_zero = board[c1[0] - 1][c1[1]] == 0 and board[c2[0] - 1][c2[1]] == 0 if c1[0] - 1 >= 0 and c2[0] - 1 >= 0 else False
+    is_left_zero = board[c1[0]][c1[1] - 1] == 0 and board[c2[0]][c2[1] - 1] == 0 if c1[1] - 1 >= 0 and c2[1] - 1 >= 0 else False
+    is_right_zero = board[c1[0]][c1[1] + 1] == 0 and board[c2[0]][c2[1] + 1] == 0 if c1[1] + 1 < len(board) and c2[1] + 1 < len(board) else False
+    lying = c1[0] == c2[0]    
     # 우측 이동 경우
-    if movement == 0:
-        if board[c1[0]][c1[1] + 1] == 0 and board[c2[0]][c2[1] + 1] == 0: 
-            next_pos_set = set((c1[0], c1[1] + 1), (c2[0], c2[1] + 1))
+    if movement == 0 or movement == 1:
+        if c1[1] + 1 < len(board) and c2[1] + 1 < len(board) and board[c1[0]][c1[1] + 1] == 0 and board[c2[0]][c2[1] + 1] == 0: 
+            next_pos = ((c1[0], c1[1] + 1), (c2[0], c2[1] + 1))
             result = True
     # 아래 이동
-    elif movement == 1: 
-        if board[c1[0] + 1][c1[1]] == 0 and board[c2[0] + 1][c2[1]] == 0: 
-            next_pos_set = set((c1[0] + 1, c1[1]), (c2[0] + 1, c2[1]))
-            result = True
-    # 아래 두개가 0이어야 하는 경우
     elif movement == 2 or movement == 3:
-        is_bottom_zero = board[c1[0] + 1][c1[1]] == 0 and board[c2[0] + 1][c2[1]] == 0
-        lying = c1[0] == c2[0]
+        if c1[0] + 1 < len(board) and c2[0] + 1 < len(board) and board[c1[0] + 1][c1[1]] == 0 and board[c2[0] + 1][c2[1]] == 0:
+            next_pos = ((c1[0] + 1, c1[1]), (c2[0] + 1, c2[1]))
+            result = True
+    # 위로 이동
+    # 아래 두개가 0이어야 하는 경우
+    elif movement == 4 or movement == 5:
         if is_bottom_zero and lying:
             result = True
-            if movement == 2:
+            if movement == 4:
                 # 시작 가로 / 우측 기준 반시계 회전
-                next_pos_set = set((c2[0], c2[1]), (c2[0] + 1, c2[1]))
-            elif movement == 3:
+                next_pos = ((c2[0], c2[1]), (c2[0] + 1, c2[1]))
+            elif movement == 5:
                 # 시작 가로 / 좌측 기준 시계 회전
-                next_pos_set = set((c1[0], c1[1]), (c1[0] + 1, c1[1]))
+                next_pos = ((c1[0], c1[1]), (c1[0] + 1, c1[1]))
         
     # 위에 두개가 0이어야 하는 경우
-        # 시작 가로 / 우측 기준 시계 회전
-        # 시작 가로 / 좌측 기준 반시계 회전
+    elif movement == 6 or movement == 7:
+        if is_top_zero and lying:
+            result = True
+            if movement == 6:
+            # 시작 가로 / 우측 기준 시계 회전
+                next_pos = ((c2[0] - 1, c2[1]), (c2[0], c2[1]))      
+            elif movement == 7:
+            # 시작 가로 / 좌측 기준 반시계 회전
+                next_pos = ((c1[0] - 1, c1[1]), (c1[0], c1[1]))      
     
     # 우측 두개가 0이어야 하는 경우
-        # 시작 세로 / 위 기준 반시계 회전
-        # 시작 세로 / 아래 기준 시계 회전
+    elif movement == 8 or movement == 9:
+        if is_right_zero and not lying:
+            result = True
+            if movement == 8:
+                # 시작 세로 / 위 기준 반시계 회전
+                next_pos = ((c1[0], c1[1]), (c1[0], c1[1] + 1))    
+            elif movement == 9:
+                # 시작 세로 / 아래 기준 시계 회전
+                next_pos = ((c2[0], c2[1]), (c2[0], c2[1] + 1))    
     
     # 좌측 두개가 0이어야 하는 경우
-        # 시작 세로 / 위 기준 시계 회전
-        # 시작 세로 / 아래 기준 반시계 회전
-    
-    return True, next_pos_set
+    elif movement == 10 or movement == 11:
+        if is_left_zero and not lying:
+            result = True
+            if movement == 10:
+                # 시작 세로 / 위 기준 시계 회전
+                next_pos = ((c1[0], c1[1] - 1), (c1[0], c1[1]))    
+            elif movement == 11:
+                # 시작 세로 / 아래 기준 반시계 회전
+                next_pos = ((c2[0], c2[1] - 1), (c2[0], c2[1]))    
+    # 좌측 이동 경우
+    elif movement == 12 or movement == 13:
+        if c1[1] - 1 >= 0 and c2[1] - 1 >= 0 and board[c1[0]][c1[1] - 1] == 0 and board[c2[0]][c2[1] - 1] == 0: 
+            next_pos = ((c1[0], c1[1] - 1), (c2[0], c2[1] - 1))
+            result = True
+    # 위 이동
+    elif movement == 14 or movement == 15:
+        if c1[0] - 1 >= 0 and c2[0] - 1 >= 0 and board[c1[0] - 1][c1[1]] == 0 and board[c2[0] - 1][c2[1]] == 0:
+            next_pos = ((c1[0] - 1, c1[1]), (c2[0] - 1, c2[1]))
+            result = True
+    return result, next_pos
 
 def solution(board):
     answer = 0
     N = len(board)
-    pos = set([(0,0), (0,1)])
+    pos = ((0,0), (0,1))
     cost = 0
     q = deque([(pos, cost)])
-    visited = set(tuple(pos))
-    print(q)
-    while True:
-        pos_set, cost = q.popleft()
-        
-        if pos_set == {(N - 1, N - 1), (N - 1, N - 2)} or pos_set == {(N - 1, N - 1), (N - 2, N - 1)}:
+    visited = set([pos])
+    while q:
+        pos, cost = q.popleft()
+        if pos == ((N - 1, N - 2), (N - 1, N - 1)) or pos == ((N - 2, N - 1), (N - 1, N - 1)):
             answer = cost
             break
         
-        c1, c2 = list(pos_set)
-        movable_poses = []
-        # 우측 이동
-        movable_poses.append(set((c1[0], c1[1] + 1), (c2[0], c2[1] + 1)))
-        # 아래 이동
-        movable_poses.append(set((c1[0] + 1, c1[1]), (c2[0] + 1, c2[1])))
-        # 우측 기준 반시계 회전
-        movable_poses.append(set((c2[0] + 1, c2[1]), (c2[0], c2[1])))
-        # 좌측 기준 시계 회전
-        movable_poses.append(set((c1[0], c1[1]), (c1[0] + 1, c1[1])))
-        for next_pos_set in movable_poses:
-            next_pos_set = set(list(next_pos_set).sorted(key=lambda x:(x[0], x[1])))
-            if not tuple(next_pos_set) in visited and check_movable(pos_set, next_pos_set, board):
-                visited.add(tuple(next_pos_set))
-                q.append(next_pos_set, cost + 1)
+        for movement in range(16):
+            movable, next_pos = check_movable(pos, movement, board)
+            if movable and not next_pos in visited:
+                visited.add(next_pos)
+                q.append((next_pos, cost + 1))
     return answer
