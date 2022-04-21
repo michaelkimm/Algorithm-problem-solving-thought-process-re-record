9bffa9598694784ceefa813de92af364017e24b1
michael <mskim_451@dongguk.edu>
2022-04-21 18:06:24 +0900

    Update Move110.py
    알고리즘: -
    자료구조: 스택
    사고 과정
    1. 110을 뽑아서 적절한 위치에 삽입해야함.
    2. 110을 list.find로 찾으면 안됨. list 크기가 100만일 수 있기 때문에 110이 리스트 개수 / 3 만큼 리스트 내에 있으면 시간복잡도가 O(N2)이 되어 시간 초과가 남.
    3. O(nlogn)이하 시간 복잡도의 110 검사 방법을 찾아야함.
    4. 110이 나올 수 있는 경우는 처음부터 110이 있을 수 있지만, 110이 빠짐으로서 새로운 110이 나올 수도 있다. 따라서 이전 발생 문자와 이후 발생 문자를 매치해야한다. 이를 구현할 수 있는 자료구조는 스택이다. 스택 맨위에 110이 쌓이면 이를 제거할 수 있다.
    5. 제거 행위는 스택으로 해결되지만 삽입 행위는 고려해야할 사항이 있다. 제거한 110을 바로 삽입하면 이후 스택에서 들어오는 110이 제거했던 110인지 새롭게 만들어지는 110인지 판단하기가 어려운 문제가 있다. 따라서 제거한 110을 따로 관리할 필요가 있다. 그렇다면 제거한 110을 나중에 되는지 확인해보자. 110 제거는 이론상 무한대로 가능하기 때문에 110을 나중에 넣어도 문제가 되지 않는다.
    6. 110 제거와 삽입을 분리하자. 처음엔 110을 전부 제거하고 나중에 110을 전부 삽입하자. 110을그렇다면 110 삽입을 어디다가 하는지가 다음 이슈다.
    7. 110보다 클 수 있는 수는 110 뒤에 가야하고 작은 수는 앞으로 가야한다. 110보다 클 수 있는 수는 111보다 더 1이 많은 수 혹은 문자열 맨 끝에 있는 1과 11이다. 따라서 삽입 시 111이 있으면 그 앞에 110을 전부 삽입 혹은 문자열 맨 끝에 11이 있으면 그 앞에 삽입 혹은 문자열 맨 끝에 1이 있으면 그 앞에 삽입하면 된다.

--- Python/Programmers/Move110.py
+++ Python/Programmers/Move110.py
@@ -31,3 +31,41 @@ def solution(s):
     for source in s:
         answer.append(move_110(source))
     return answer
+
+
+# ============================================================= #
+
+def move_110(target):
+    pop_cnt = 0
+    stack = []
+    for num in target:
+        # 삽입
+        stack.append(num)
+        # 스택 안이 110이면 세게 다 꺼내고 pop_cnt += 1
+        if len(stack) >= 3 and stack[-3:] == ['1', '1', '0']:
+            del stack[-3:]
+            pop_cnt += 1
+    target = ''.join(stack)
+    
+    idx_111 = target.find("111")
+    string_110 = ''.join(["110" for _ in range(pop_cnt)])
+    if idx_111 == -1:
+        # 맨 끝이 11인 경우
+        if len(target) >= 2 and target[len(target) - 2:] == "11": 
+            target = target[:len(target) - 2] + string_110 + target[len(target) - 2:]
+        # 맨 끝이 1인 경우
+        elif len(target) >= 1 and target[-1] == '1':
+            target = target[:len(target) - 1] + string_110 + target[-1]
+        # 둘다 아닌 경우
+        else:
+            target += string_110
+    else:
+        target = target[:idx_111] + string_110 + target[idx_111:]
+
+    return target
+    
+def solution(s):
+    answer = []
+    for source in s:
+        answer.append(move_110(source))
+    return answer
