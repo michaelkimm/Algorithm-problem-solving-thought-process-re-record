c1ef41fac2936623a1e930d4ea6b84d2dbe3040a
michael <mskim_451@dongguk.edu>
2022-05-01 13:26:40 +0900

    Update HideAndSeek.java
    알고리즘: bfs
    자료구조: 큐
    사고 과정
    1. 숫자N이 숫자 K가 되는 최단 시간을 구하면 됨. 최단 시간이므로 bfs를 떠올림. 노드와 간선을 정의해야함. 노드는 숫자가 되고 간선은 좌우 움직임, 2배 건너띄기가 됨.
    2. 숫자 N이 K가 되는 과정을 bfs 하면 불필요한 노드가 많이 나옴. 계속 두배 커지려고 하기 때문. 대신 K가 N이 되는 과정을 생각하면 K가 짝수일 때문 2배 작아지면 됨. 따라서 텔레포트 움직임을 줄일 수 있음.

--- Java/HideAndSeek.java
+++ Java/HideAndSeek.java
@@ -38,3 +38,69 @@ class Main {
     return myFunction(bigNum, smallNum, cnt);
   }
 }
+
+// ------------------------------------------------------------- //
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.Scanner;
+
+  class Main {
+    public static class Node{
+    public int number;
+    public int count;
+    public Node() {}
+    public Node(int number, int count)
+    {
+      this.number = number;
+      this.count = count;
+    }
+  }
+  
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    int N = myScanner.nextInt();
+    int K = myScanner.nextInt();
+    int cnt = 0;
+    Queue<Node> q = new LinkedList<Node>();
+    HashSet<Integer> visited = new HashSet<Integer>();
+    if (K > N){
+      Node newNode = new Node(K, cnt);
+      q.add(newNode);
+      visited.add(newNode.number);
+    }
+      
+    else
+      cnt = N - K;
+    while (q.size() > 0){
+      Node node = q.remove();
+      // System.out.println(node.number + "- " + node.count);
+      if (node.number == N){
+        cnt = node.count;
+        break;
+      }
+      // 간선
+      Node newNode = null;
+      if (node.number % 2 == 0 && node.number != 0)
+      {
+        newNode = new Node((int)(node.number / 2), node.count + 1);
+        if (!visited.contains(newNode.number)){
+          q.add(newNode);
+          visited.add(newNode.number);
+        }
+      }  
+      newNode = new Node(node.number - 1, node.count + 1);
+      if (!visited.contains(newNode.number) && newNode.number >= 0){
+        q.add(newNode);
+        visited.add(newNode.number);
+      }
+      
+      newNode = new Node(node.number + 1, node.count + 1);
+      if (!visited.contains(newNode.number) && newNode.number <= 100000){
+        q.add(newNode);
+        visited.add(newNode.number);
+      }
+    }
+    System.out.println(cnt);
+  }
+}
