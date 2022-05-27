635e1d1977097fce4532b9d62489d0fba64de632
michael <mskim_451@dongguk.edu>
2022-05-27 11:16:10 +0900

    백준.2470.풀이_완료
    알고리즘: 투 포인터
    자료구조: -
    사고 과정
    - 풀지 못하여 답안을 참조했기에 문제 사고 과정 복기가 무의미함.
    - 문제 사고 과정 복기 대신 해당 문제를 접근하기 위한 더 나은 사고 습관을 보기함.
    
    사고 습관 변경 필요 사항
    1. 예시를 하나씩 전개해보자
    - 이때 문제의 답안 요구사항을 생각하자
    - 규칙을 찾아보자
    2. 규칙 찾은게 코드로 잘 옮겨졌는지 확인해보자
    - 실수가 있을 수 있다. 코드 한줄 한줄 뜯어보자.

--- Java/BaekJoon/2470.java
+++ Java/BaekJoon/2470.java
@@ -6,62 +6,37 @@ import java.util.stream.Collectors;
 
 class Main {
 
-  static int N;
-  static List<Long> liquids;
-  static Long[] answer;
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine());
-    liquids = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).boxed().collect(Collectors.toList());
-    Collections.sort(liquids);
+    int N = Integer.parseInt(br.readLine());
+    int[] liquids = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    Arrays.sort(liquids);
 
-    liquids.stream().forEach(System.out::println);
-    answer = new Long[2];
-
+    int ans1 = 0;
+    int ans2 = 0;
     int left = 0;
-    int right = 1;
-    long minValue = Long.MAX_VALUE;
-    // left fixed, right move right
-    while (right < liquids.size()) {
-      long mixed = getMixed(left, right);
-
-      if (mixed < minValue) {
-        minValue = mixed;
-        right += 1;
+    int right = N - 1;
+    int minValue = Integer.MAX_VALUE;
+    while (left < right) {
+      int mixed = getMixed(liquids, left, right);
+      int mixedAbs = Math.abs(mixed);
+      if (mixedAbs < minValue) {
+        minValue = mixedAbs;
+        ans1 = liquids[left];
+        ans2 = liquids[right];
       }
-      else
-        break;
-    }
-    right -= 1;
-    updateAnswer(left, right);
 
-    // left move right, right move left
-    while (left < liquids.size()) {
-      // left move right
-      left += 1;
-      // right move left until mixed is bigger than minValue
-      long mixed = getMixed(left, right);
-      while (mixed > minValue ||) {
+      if (mixed > 0)
         right -= 1;
-        mixed = getMixed(left, right);
-      }
-      right += 1;
-      updateAnswer(left, right);
+      else
+        left += 1;
     }
 
-    System.out.println("left = " + left);
-    System.out.println("right = " + right);
-    System.out.println("minValue = " + minValue);
+    System.out.println(ans1 + " " + ans2);
   }
 
-  private static void updateAnswer(int left, int right) {
-    answer[0] = liquids.get(left);
-    answer[1] = liquids.get(right);
-  }
-
-  private static long getMixed(int left, int right) {
-    long mixed = Math.abs(liquids.get(left) + liquids.get(right));
-    return mixed;
+  private static int getMixed(int[] liquids, int left, int right) {
+    return liquids[left] + liquids[right];
   }
 }
