8e0974adb6f547ba45f2c73e72938bee606bbded
michael <mskim_451@dongguk.edu>
2022-06-03 11:25:49 +0900

    백준.2143.풀이중
    메모리 초과 해결 필요
    원인: 해시맵, arrayList

--- Java/BaekJoon/2143.java
+++ Java/BaekJoon/2143.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 class Main {
@@ -23,42 +24,88 @@ class Main {
     }
 
     int[] cumulativeA = getCumulative(A);
-    List<Integer> ccA = getRangeSum(cumulativeA);
+    int[] ccA = getRangeSum(cumulativeA);
     int[] cumulativeB = getCumulative(B);
+    int[] ccB = getRangeSum(cumulativeB);
 
-    System.out.println("ca");
-    printAry(cumulativeA);
+//    System.out.println("cca");
+//    printAry(ccA);
+//
+//    System.out.println("ccb");
+//    printAry(ccB);
 
-    System.out.println("cca");
-    printAry(ccA);
+    Map<Integer, Integer> ccAfreqMap = getNumberFrequency(ccA);
+    Map<Integer, Integer> ccBfreqMap = getNumberFrequency(ccB);
 
-    System.out.println("cb");
-    printAry(cumulativeB);
+    Set<Integer> ccANumSet = new HashSet<>(Arrays.stream(ccA).boxed().collect(Collectors.toList()));
+    List<Integer> ccAUniqNums = ccANumSet.stream().collect(Collectors.toList());
+    Collections.sort(ccAUniqNums);
+    Set<Integer> ccBNumSet = new HashSet<>(Arrays.stream(ccB).boxed().collect(Collectors.toList()));
+    List<Integer> ccBUniqNums = ccBNumSet.stream().collect(Collectors.toList());
+    Collections.sort(ccBUniqNums);
+
+//    System.out.println("ccAUniqNums");
+//    printAry(ccAUniqNums);
+//    printMap(ccAfreqMap);
+//    System.out.println();
+//
+//
+//    System.out.println("ccBUniqNums");
+//    printAry(ccBUniqNums);
+//    printMap(ccBfreqMap);
+//    System.out.println();
 
     int answer = 0;
+    int left = 0;
+    int right = ccBUniqNums.size() - 1;
+    while (left < ccAUniqNums.size() && right >= 0) {
+      int leftNum = ccAUniqNums.get(left);
+      int rightNum = ccBUniqNums.get(right);
+      int sum = leftNum + rightNum;
+      if (sum == T) {
+        answer += ccAfreqMap.get(leftNum) * ccBfreqMap.get(rightNum);
+        right -= 1;
+      } else if (sum > T) {
+        right -= 1;
+      } else if (sum < T) {
+        left += 1;
+      }
+    }
+
     System.out.println(answer);
   }
 
-  private static List<Integer> getRangeSum(int[] ary) {
+  private static int[] getRangeSum(int[] ary) {
     int N = ary.length;
-    ArrayList<Integer> result = new ArrayList<>();
+    int[] result = new int[(N * (N - 1)) / 2];
+    int idx = 0;
     for (int left = 0; left < N; left++) {
       for (int right = left + 1; right < N; right++) {
-        result.add(ary[right] - ary[left]);
+        result[idx++] = (ary[right] - ary[left]);
       }
     }
-    Collections.sort(result);
+    Arrays.sort(result);
     return result;
   }
 
   private static int[] getCumulative(int[] ary) {
-    int[] cumulative = new int[ary.length];
-    for (int i = 0; i < ary.length; i++) {
-      if (i != 0)
-        cumulative[i] = cumulative[i - 1];
-      cumulative[i] += ary[i];
+    int[] cumulative = new int[ary.length + 1];
+    for (int i = 1; i <= ary.length; i++) {
+      cumulative[i] = cumulative[i - 1];
+      cumulative[i] += ary[i - 1];
     }
     return cumulative;
+  }
+
+  private static Map<Integer, Integer> getNumberFrequency(int[] ary) {
+    HashMap<Integer, Integer> freqMap = new HashMap<>();
+    for (int number : ary) {
+      if (freqMap.containsKey(number))
+        freqMap.put(number, freqMap.get(number) + 1);
+      else
+        freqMap.put(number, 1);
+    }
+    return freqMap;
   }
 
   private static void printAry(int[] ary) {
@@ -73,5 +120,10 @@ class Main {
       System.out.print(ary.get(i) + " ");
     }
     System.out.println();
+  }
+  private static void printMap(Map<Integer, Integer> map) {
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      System.out.println("Key = " + entry.getKey() + ",Value = " + entry.getValue());
+    }
   }
 }
