98ba76166ef604d8aa4fb547353421e972bcfb25
michael <mskim_451@dongguk.edu>
2022-06-04 12:53:40 +0900

    백준.2143.풀이_완료
    알고리즘: 누적 합, 투 포인터
    자료구조: 배열
    사고 과정
    1. 연속된 원소 합은 누적합으로 접근하는게 연산에 효율적임.
    2. 누적 합 배열에서 투 포인터로 특정 원소를 O(N) 시간 내 찾을 수 있음. 하지만 우리는 특정 원소 갯수까지 알아야함. 이는 투 포인터로 알아낼 수 없음. 현재 상태에서 투 포인터는 특정 수의 존재 유무만 파악할 수 있기 때문.
    3. 특정 원소 갯수를 아는 방법엔 여러가지가 있겠지만 가장 간단한 방법으로는 미리 원소들을 모두 구해놓은 방법이 있음. 원소 개수가 최대 1000개이기 때문에 미리 원소를 모두 구해도 100만을 절대 넘지 않음. 따라서 미리 원소를 모두 구한 후 정렬을 해놓자.
    4. 미리 구한 원소에서 특정 조건을 만족하는 원소 쌍을 구해야함. 중복 원소의 존재를 생각하며 투 포인터를 사용하면 O(N) 시간 내 문제를 풀 수 있음. 구간 내 조건을 만족하는 것을 선형 시간 내 처리해야 하기 때문에 투 포인터를 사용함.

--- Java/BaekJoon/2143.java
+++ Java/BaekJoon/2143.java
@@ -8,76 +8,70 @@ import java.util.stream.Collectors;
 class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
+    long T = Long.parseLong(br.readLine());
     int N = Integer.parseInt(br.readLine());
     String[] ns = br.readLine().split(" ");
-    int[] A = new int[N];
+    long[] A = new long[N];
     for (int i = 0; i < N; i++) {
-      A[i] = Integer.parseInt(ns[i]);
+      A[i] = Long.parseLong(ns[i]);
     }
 
     int M = Integer.parseInt(br.readLine());
     String[] ms = br.readLine().split(" ");
-    int[] B = new int[M];
+    long[] B = new long[M];
     for (int i = 0; i < M; i++) {
-      B[i] = Integer.parseInt(ms[i]);
+      B[i] = Long.parseLong(ms[i]);
     }
 
-    int[] cumulativeA = getCumulative(A);
-    int[] ccA = getRangeSum(cumulativeA);
-    int[] cumulativeB = getCumulative(B);
-    int[] ccB = getRangeSum(cumulativeB);
+    long[] cumulativeA = getCumulative(A);
+    long[] ccA = getRangeSum(cumulativeA);
+    long[] cumulativeB = getCumulative(B);
+    long[] ccB = getRangeSum(cumulativeB);
 
-//    System.out.println("cca");
-//    printAry(ccA);
-//
-//    System.out.println("ccb");
-//    printAry(ccB);
-
-    Map<Integer, Integer> ccAfreqMap = getNumberFrequency(ccA);
-    Map<Integer, Integer> ccBfreqMap = getNumberFrequency(ccB);
-
-    Set<Integer> ccANumSet = new HashSet<>(Arrays.stream(ccA).boxed().collect(Collectors.toList()));
-    List<Integer> ccAUniqNums = ccANumSet.stream().collect(Collectors.toList());
-    Collections.sort(ccAUniqNums);
-    Set<Integer> ccBNumSet = new HashSet<>(Arrays.stream(ccB).boxed().collect(Collectors.toList()));
-    List<Integer> ccBUniqNums = ccBNumSet.stream().collect(Collectors.toList());
-    Collections.sort(ccBUniqNums);
-
-//    System.out.println("ccAUniqNums");
-//    printAry(ccAUniqNums);
-//    printMap(ccAfreqMap);
-//    System.out.println();
-//
-//
-//    System.out.println("ccBUniqNums");
-//    printAry(ccBUniqNums);
-//    printMap(ccBfreqMap);
-//    System.out.println();
-
-    int answer = 0;
+    long answer = 0;
     int left = 0;
-    int right = ccBUniqNums.size() - 1;
-    while (left < ccAUniqNums.size() && right >= 0) {
-      int leftNum = ccAUniqNums.get(left);
-      int rightNum = ccBUniqNums.get(right);
-      int sum = leftNum + rightNum;
+    int right = ccB.length - 1;
+    int leftCnt = 0;
+    int rightCnt = 0;
+    while (left < ccA.length && right >= 0) {
+      long sum = ccA[left] + ccB[right];
       if (sum == T) {
-        answer += ccAfreqMap.get(leftNum) * ccBfreqMap.get(rightNum);
+        // move right
+        while (sum == T) {
+          rightCnt += 1;
+          right -= 1;
+          if (right < 0)
+            break;
+          sum = ccA[left] + ccB[right];
+        }
+        right += 1;
+        // move left
+        sum = ccA[left] + ccB[right];
+        while (sum == T) {
+          leftCnt += 1;
+          left += 1;
+          if (left >= ccA.length)
+            break;
+          sum = ccA[left] + ccB[right];
+        }
+        left -= 1;
+        answer += ((long)rightCnt * (long)leftCnt);
         right -= 1;
       } else if (sum > T) {
         right -= 1;
       } else if (sum < T) {
         left += 1;
       }
+      leftCnt = 0;
+      rightCnt = 0;
     }
 
     System.out.println(answer);
   }
 
-  private static int[] getRangeSum(int[] ary) {
+  private static long[] getRangeSum(long[] ary) {
     int N = ary.length;
-    int[] result = new int[(N * (N - 1)) / 2];
+    long[] result = new long[(N * (N - 1)) / 2];
     int idx = 0;
     for (int left = 0; left < N; left++) {
       for (int right = left + 1; right < N; right++) {
@@ -88,42 +82,12 @@ class Main {
     return result;
   }
 
-  private static int[] getCumulative(int[] ary) {
-    int[] cumulative = new int[ary.length + 1];
+  private static long[] getCumulative(long[] ary) {
+    long[] cumulative = new long[ary.length + 1];
     for (int i = 1; i <= ary.length; i++) {
       cumulative[i] = cumulative[i - 1];
       cumulative[i] += ary[i - 1];
     }
     return cumulative;
-  }
-
-  private static Map<Integer, Integer> getNumberFrequency(int[] ary) {
-    HashMap<Integer, Integer> freqMap = new HashMap<>();
-    for (int number : ary) {
-      if (freqMap.containsKey(number))
-        freqMap.put(number, freqMap.get(number) + 1);
-      else
-        freqMap.put(number, 1);
-    }
-    return freqMap;
-  }
-
-  private static void printAry(int[] ary) {
-    for (int i = 0; i < ary.length; i++) {
-      System.out.print(ary[i] + " ");
-    }
-    System.out.println();
-  }
-
-  private static void printAry(List<Integer> ary) {
-    for (int i = 0; i < ary.size(); i++) {
-      System.out.print(ary.get(i) + " ");
-    }
-    System.out.println();
-  }
-  private static void printMap(Map<Integer, Integer> map) {
-    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-      System.out.println("Key = " + entry.getKey() + ",Value = " + entry.getValue());
-    }
   }
 }
