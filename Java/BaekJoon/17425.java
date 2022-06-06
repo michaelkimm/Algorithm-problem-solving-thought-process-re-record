e1d695120d6cb443294ceee15d4c09b9e556dcd3
michael <mskim_451@dongguk.edu>
2022-06-06 09:59:09 +0900

    백분.17425.풀이_완료
    알고리즘: 에라토스테네스의 체, 누적합(dp)
    자료구조: 배열
    사고 과정
    1. g(x)는 연속된 f(x)의 값의 합을 통해 얻을 수 있다. f(x)를 누적합 형식으로 구해 놓으면 O(1)로 g(x)를 구할 수 있다. 따라서 가장 큰 입력 수 보다 작거나 같은 수의 f(x)의 값을 구하자.
    2. f(x)의 값을 루트x이하의 약수 구하기 방법을 사용하여 구하면 O(루트n)에 구할 수 있다. 하지만 이 방법을 사용하면 시그마루트n이 되기 때문에 최악의 경우 6억번 연산을 해야한다. 따라서 f(x) 값을 더 빠르게 구해야한다.
    3. f(x) 값을 더 빠르게 구하기 위해 예시를 하나씩 펼쳐가보자. 이렇게한다면 규칙이 보이고 이는 에라토스테네스의 체라는 것을 알 수 있다. 에라토스테네스의 체는 O(NloglogN)이기 때문에 시그마루트n보다 빠르다.
    4. 입력 및 출력이10만번씩 일어나기 때문에 입출력도 최적화하기 위해 buffered reader를 사용하자. system.out.println은 스레드를 뺏기기 때문에 스레드 스위칭 시간이 추가되기 때문이다.

--- Java/BaekJoon/17425.java
+++ Java/BaekJoon/17425.java
@@ -10,40 +10,35 @@ class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int T = Integer.parseInt(br.readLine().strip());
     int[] numbers = new int[T];
+
+
     for (int i = 0; i < T; i++) {
       int value = Integer.parseInt(br.readLine());
       numbers[i] = value;
     }
-    Arrays.sort(numbers);
 
-    int maxNumber = numbers[numbers.length - 1];
-    long[] f = new long[maxNumber + 1];
-    for (int num = 1; num <= maxNumber; num++) {
-      f[num] = getDivisorSum((long)num);
+    int MAX_VALUE = Arrays.stream(numbers).max().getAsInt() + 1;
+
+    // get fx
+    long[] f = new long[MAX_VALUE];
+    Arrays.fill(f, 1L);
+    for (int num = 2; num < f.length; num++) {
+      for (int i = 1; i * num < f.length; i++) {
+        f[num * i] += num;
+      }
     }
 
     // cumulative sum
-    long[] cumulativeSum = new long[maxNumber + 1];
-    for (int num = 1; num <= maxNumber; num++) {
-      cumulativeSum[num] = cumulativeSum[num - 1];
-      cumulativeSum[num] += f[num];
+    long[] cumulativeSum = new long[MAX_VALUE];
+    for (int num = 1; num < cumulativeSum.length; num++) {
+      cumulativeSum[num] += (cumulativeSum[num - 1] + f[num]);
     }
 
+    StringBuffer sb = new StringBuffer();
     for (int i = 0; i < numbers.length; i++) {
-      System.out.println(cumulativeSum[numbers[i]]);
+//      System.out.println(cumulativeSum[numbers[i]]);
+      sb.append(cumulativeSum[numbers[i]]).append("\n");
     }
-  }
-
-  static long getDivisorSum(long number) {
-    long sum = 0L;
-    for (long divNum = 1; divNum * divNum <= number; divNum++) {
-      if (number % divNum == 0) {
-        if (divNum == number / divNum)
-          sum += divNum;
-        else
-          sum += (divNum + number / divNum);
-      }
-    }
-    return sum;
+    System.out.println(sb);
   }
 }
