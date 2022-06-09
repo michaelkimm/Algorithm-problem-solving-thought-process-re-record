57765d37377e7c06f8bf578ae8919b93b32fd08c
michael <mskim_451@dongguk.edu>
2022-06-09 15:14:19 +0900

    백준.2904.풀이_완료
    알고리즘: 에라스토테네스의 체, 구현
    자료구조: 배열
    사고 과정
    1. 주어지는 숫자 개수는 100이다. 그러므로 한 숫자당 100만번의 연산 기회가 주어진다고 볼 수 있다.
    2. 한 숫자는 최대 100만이 될 수 있다. 우리는 한 숫자가 최대 공약수가 되기 위해 움직일 횟수를 알아야한다. 이를 알기 위해 숫자의 소인수 분해된 결과를 알아야한다.
    3 숫자의 소인수 분해 결과를 저장할 자료구조를 생각해보자. 몇번째 숫자, 어떤 소인수, 제곱 수 세개의 데이터를 저장할 수 있어야한다. 배열로 가능한지 알아보자. '[몇번째 숫자][어떤 소인수] = 제곱수'로 표현할 수 있다. 배열로 결정해보자.
    4. 배열로 결정하면 시간복잡도가 맞을까? 100만 이하의 소수 개수는 8만개정도 된다. 따라서 배열을 이용하면 전체 시간복잡도는 최악의 경우 100 * 8만이다. 즉 800만이므로 시간제한을 충분히 만족시킨다.
    5. 공간복잡도를 생각해보자. 모든 숫자의 소인수 분해 결과를 저장하는 2차원 배열은 최악의 경우 [100][80000]이 된다. 이는 32000000바이트 = 32바이트다. gcd 결과를 저장하는 공간은 위와 같이 계산하면 0.8바이트다. 따라서 공간복잡도 또한 문제 없다.

--- Java/BaekJoon/2904.java
+++ Java/BaekJoon/2904.java
@@ -7,142 +7,62 @@ import java.util.stream.Collectors;
 
 class Main {
 
-  static long[] primeNumbers;
+  static List<Integer> primeNumbers;
+  static int[] gcd;
+  static int[][] pfOfNumbers;
 
   public static void main(String[] args) throws IOException {
     // input
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     String[] values = br.readLine().split(" ");
-    long[] numbers = new long[N];
+    int[] numbers = new int[N];
     for (int i = 0; i < numbers.length; i++) {
-      numbers[i] = Long.parseLong(values[i]);
+      numbers[i] = Integer.parseInt(values[i]);
     }
+    int maxValue = Arrays.stream(numbers).max().getAsInt();
 
-    // initialize
-    long maxNumber = Arrays.stream(numbers).max().getAsLong();
-
-    // get prime numbers under maxNumber
-    primeNumbers = getPrimeNumberUnder(maxNumber);
-
-    Map<Long, Long> primeFactors = new HashMap<>();
-    for (int i = 0; i < N; i++) {
-      Map<Long, Long> v = getPrimeFactorsOf(numbers[i]);
-      primeFactors = getPrimeFactorMultiplied(primeFactors, v);
-    }
-
-    // get dividableFactor
-    Map<Long, Long> dividableFactor = getDividableFactor(primeFactors, N);
-
-    // fix number
-    long moveCnt = 0L;
-    for (int i = 0; i < N; i++) {
-      Map<Long, Long> v = getPrimeFactorsOf(numbers[i]);
-      moveCnt += getFixNeededCnt(v, dividableFactor);
-    }
-
-    System.out.println(getPrimeFactorToNumber(dividableFactor) + " " + moveCnt);
-  }
-
-  static Map<Long, Long> getPrimeFactorMultiplied(Map<Long, Long> primeFactor1, Map<Long, Long> primeFactor2) {
-    Map<Long, Long> result = new HashMap<>();
-    for (Map.Entry<Long, Long> v1 : primeFactor1.entrySet()) {
-      if (primeFactor2.containsKey(v1.getKey())) {
-        result.put(v1.getKey(), v1.getValue() + primeFactor2.get(v1.getKey()));
-      }
-      else
-        result.put(v1.getKey(), v1.getValue());
-    }
-
-    for (Map.Entry<Long, Long> v2 : primeFactor2.entrySet()) {
-      if (!primeFactor1.containsKey(v2.getKey()))
-        result.put(v2.getKey(), v2.getValue());
-    }
-    return result;
-  }
-
-  static long getFixNeededCnt(Map<Long, Long> target, Map<Long, Long> dividableFactor) {
-    long divCnt = 0L;
-    for (Map.Entry<Long, Long> divadable_v : dividableFactor.entrySet()) {
-      divCnt += divadable_v.getValue();
-    }
-
-    long haveCnt = 0;
-    for (Map.Entry<Long, Long> target_v : target.entrySet()) {
-      for (Map.Entry<Long, Long> divadable_v : dividableFactor.entrySet()) {
-        if (target_v.getKey() == divadable_v.getKey()) {
-          haveCnt += (target_v.getValue() >= divadable_v.getValue() ? divadable_v.getValue() : target_v.getValue());
-          break;
+    // get GCD in primeFactor
+    // get primeFactor of Numbers
+    int gcdNum = 1;
+    primeNumbers = getPrimeNumbersUnder(maxValue);
+    gcd = new int[primeNumbers.size()];
+    pfOfNumbers = new int[N][primeNumbers.size()];
+    System.out.println("primeNumbers.size: " + primeNumbers.size());
+    for (int j = 0; j < primeNumbers.size(); j++) {
+      for (int i = 0; i < N; i++) {
+        int number = numbers[i];
+        while (number % primeNumbers.get(j) == 0) {
+          number /= primeNumbers.get(j);
+          gcd[j]++;
+          pfOfNumbers[i][j]++;
         }
       }
+      gcd[j] /= N;
+      gcdNum *= Math.pow(primeNumbers.get(j), gcd[j]);
     }
 
-    return divCnt - haveCnt;
-  }
-
-  static long getPrimeFactorToNumber(Map<Long, Long> primeFacetor) {
-    long result = 1;
-    for (Map.Entry<Long, Long> v : primeFacetor.entrySet()) {
-      result *= (long)Math.pow(v.getKey(), v.getValue());
-    }
-    return result;
-  }
-
-  static void printMap(Map<Long, Long> map) {
-    for (Map.Entry<Long, Long> v : map.entrySet()) {
-      System.out.println(v.getKey() + "^" + v.getValue());
-    }
-    System.out.println();
-  }
-
-  static Map<Long, Long> getDividableFactor(Map<Long, Long> primeFactors, int N) {
-    Map<Long, Long> result = new HashMap<>();
-    for (Map.Entry<Long, Long> pf : primeFactors.entrySet()) {
-      if (pf.getValue() / (long)N >= 1L) {
-        result.put(pf.getKey(), pf.getValue() / (long)N);
+    // get move count
+    int count = 0;
+    for (int idx = 0; idx < N; idx++) {
+      for (int j = 0; j < primeNumbers.size(); j++) {
+        if (pfOfNumbers[idx][j] < gcd[j])
+          count += (gcd[j] - pfOfNumbers[idx][j]);
       }
     }
-    return result;
+    System.out.println(gcdNum + " " + count);
   }
 
-  static Map<Long, Long> getPrimeFactorsOf(long number) {
-    Map<Long, Long> result = new HashMap<>();
-    for (long primeNumber : primeNumbers) {
-      while (number % primeNumber == 0) {
-        number /= primeNumber;
-        if (result.containsKey(primeNumber))
-          result.replace(primeNumber, result.get(primeNumber) + 1L);
-        else
-          result.put(primeNumber, 1L);
-      }
-    }
-    return result;
-  }
+  static List<Integer> getPrimeNumbersUnder(int numberBorder) {
+    List<Integer> result = new ArrayList<>();
+    boolean[] isPrimeNumber = new boolean[numberBorder + 1];
 
-  static long[] getPrimeNumberUnder(long maxNumber) {
-    long[] result;
-    // eratotaenes che
-    boolean[] primeAvailables = new boolean[(int)(maxNumber + 1L)];
-    Arrays.fill(primeAvailables, true);
-    primeAvailables[0] = false;
-    primeAvailables[1] = false;
-    for (long num = 2L; num * num <= maxNumber; num++) {
-      if (!primeAvailables[(int) num])
-        continue;
-      for (long multiple = num * num; multiple <= maxNumber; multiple += num) {
-        primeAvailables[(int)multiple] = false;
-      }
-    }
-    int primeCnt = 0;
-    for (boolean primeAvailable : primeAvailables) {
-      if (primeAvailable)
-        primeCnt += 1;
-    }
-    result = new long[primeCnt];
-    int idx = 0;
-    for (int i = 2; i < primeAvailables.length; i++) {
-      if (primeAvailables[i]) {
-        result[idx++] = (long)i;
+    for (int num = 2; num <= numberBorder; num++) {
+      if (!isPrimeNumber[num]) {
+        result.add(num);
+        for (int multipled = num * 2; multipled <= numberBorder; multipled += num) {
+          isPrimeNumber[multipled] = true;
+        }
       }
     }
     return result;
