9744cb2fa8fc255f3f1bac1a2ad52cf07653748a
michael <mskim_451@dongguk.edu>
2022-06-28 21:34:15 +0900

    프로그래머스.카드짝맞추기.두번째풀이_완료
    <사고 과정>
    - 목표는 남은 카드를 모두 제거하는데 필요한 키 조작 횟수의 최솟값을 구하는것
    - 격자, 움직임 최솟값 구하기이기 때문에 BFS 떠올림.
    - 노드와 간선을 정의해야함. 간선은 cost를 올릴 수 있는 방향키, Ctrl + 방향키, Enter가 될 수 있음.
    - 엔터가 간선이기 때문에 노드는 하나 하나 뒤집은 것 까지 정의됨.
        - 노드: 현재 위치, 판 상태(현재까지 오게 된 경로는 중요하지 않음. 우린 최소 cost만 알면 되기 때문)
        - 간선: 방향키, Ctrl + 방향키, Enter
    
    시간 복잡도
    - 노드 정의가 복잡해서 알기가 어려움
    - 통과할 수 있는 알고리즘 중  경우가 시간 복잡도 조건을 통과하면 그 보다 효율적인 알고리즘은 무조건 통과함
        - 가장 시간 복잡도 큰 경우
            - 카드 두장 뒤집기 (24 + 16) * 6 = 240
            - 카드 순서 6!  * 2^6
            - 결과: 1천만
    - bfs는 무조건 통과함. 찾으면 다른 경우를 고려하지 않고 무조건 끝내기 때문.
    
    <성찰>
    1. 구현한 사항들을 유닛 테스트 처럼 하나씩 검증해보고 해야함
    2. 어떤 예외 사항이 있을지 꼼꼼히 따져보고 구현 시작하자.

---
+++ Python/Programmers/MatchingTheCard2ndSolv.py
@@ -0,0 +1,114 @@
+from collections import deque
+
+def getCardSetCnt(board):
+    need_erased_cnt = 0
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] != 0:
+                need_erased_cnt += 1
+    need_erased_cnt //= 2
+    return need_erased_cnt
+
+def clone2DBoardTo1D(board):
+    return ''.join([str(board[row][col]) for row in range(4) for col in range(4)])
+
+def clone1DBoardTo1D(board):
+    return ''.join([str(board[idx]) for idx in range(16)])
+
+def moveOneDist(cur_r, cur_c, dr, dc):
+    next_r = cur_r + dr
+    next_c = cur_c + dc
+    return next_r, next_c
+
+def moveWithCtrl(board, cur_r, cur_c, dr, dc):
+    next_r, next_c = cur_r, cur_c
+    while True:
+        next_r += dr
+        next_c += dc
+        if not(0 <= next_r < 4 and 0 <= next_c < 4):
+            next_r -= dr
+            next_c -= dc
+            break
+        if board[get1DIndex(next_r, next_c)] != '0':
+            break
+    if next_r == cur_r and next_c == cur_c:
+      return -1, -1
+    return next_r, next_c
+
+def get1DIndex(row, col):
+  return row * 4 + col
+
+def solution(board, r, c):
+    need_erased_cnt = getCardSetCnt(board)
+    
+    # 동서남북
+    dr = [0, 0, 1, -1]
+    dc = [-1, 1, 0, 0]
+    
+    cost = 0
+    cur_board = clone2DBoardTo1D(board)
+    erased = 0
+    entered = '0'
+    enteredIdx = -1
+    start_node = (cur_board, r, c, cost, erased, entered, enteredIdx)
+    q = deque([start_node])
+    visited = set()
+
+    cnt = 0
+    
+    while q:
+        cur_node = q.popleft()
+        cur_board, cur_r, cur_c, cur_cost, erased, entered, enteredIdx= cur_node
+        indexOf1D = get1DIndex(cur_r, cur_c)
+        # 탈출 조건
+        if (erased == need_erased_cnt):
+            cost = cur_cost
+            break
+            
+        if (cur_board, cur_r, cur_c, entered, enteredIdx) in visited:
+            continue
+        else:
+            visited.add((cur_board, cur_r, cur_c, entered, enteredIdx))
+
+        # 한칸 방향키
+        for index in range(4):
+            next_r, next_c = moveOneDist(cur_r, cur_c, dr[index], dc[index])
+            # 나간 경우
+            if not(0 <= next_r < 4 and 0 <= next_c < 4):
+                continue
+            
+            # 큐 삽입
+            next_board = clone1DBoardTo1D(cur_board)
+            q.append((next_board, next_r, next_c, cur_cost + 1, erased, entered, enteredIdx))
+        
+        # Ctrl + 방향키
+        for index in range(4):
+            next_r, next_c = moveWithCtrl(cur_board, cur_r, cur_c, dr[index], dc[index])
+            if next_r == -1 and next_c == -1:
+                continue
+            
+            # 큐 삽입
+            next_board = clone1DBoardTo1D(cur_board)
+            q.append((next_board, next_r, next_c, cur_cost + 1, erased, entered, enteredIdx))
+        
+        # Enter
+        # 빈칸 위면 패스
+        if cur_board[indexOf1D] == '0':
+            continue
+        # 엔터가 되있으면 같은 숫자만 엔터,
+        elif cur_board[indexOf1D] != '0' and cur_board[indexOf1D] == entered and enteredIdx != indexOf1D:
+            next_board = clone1DBoardTo1D(cur_board)
+            next_board = next_board.replace(cur_board[indexOf1D], '0')
+            
+            entered = '0'
+            erased += 1
+            q.append((next_board, cur_r, cur_c, cur_cost + 1, erased, entered, enteredIdx))
+        # 엔터가 안되있으면 그냥 해도 됨.
+        elif cur_board[indexOf1D] != '0' and entered == '0':
+            entered = cur_board[indexOf1D]
+            enteredIdx =indexOf1D
+            next_board = clone1DBoardTo1D(cur_board)
+            q.append((next_board, cur_r, cur_c, cur_cost + 1, erased, entered, enteredIdx))
+
+    answer = cost
+    return answer
