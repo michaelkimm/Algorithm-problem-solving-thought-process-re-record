d95bcf4829abb89a0207f29c87d2c3f3b5f05668
michael <mskim_451@dongguk.edu>
2022-08-17 13:00:20 +0900

    백준.20058.풀이완료
    알고리즘 : 구현
    자료구조 : -
    사고 과정
    1. 문제를 접했을 때 구현해야 하는 함수는 회전, melt, 최대 그룹 찾기 함수 세개였습니다. 우선 회전 함수 부터 구해야 겠다고 판단했습니다. 함수를 하나씩 구현하면서 검증해나가는 프로세스죠. 회전 함수를 구하는 것은 쉽지만 배열 내 나눠진 배열 구역을 회전하는 것이 문제였습니다. 이에 먼저 구역을 추상적으로 표현하려고 했습니다. 각 구역은 좌측 위 꼭지점과 2차원 배열 크기로 표현할 수 있었습니다. 결국 추상화된 나눠진 배열 요소들의 list를 얻게 됐으며, 이후 transpose와 reverse를 이용하여 90 도 회전을 구현했습니다.
    
    2. 두번째로 구현할 함수는  melt였습니다. melt가 한번에 이뤄져야하기 때문에 melt 될 후보를 list에 담았습니다. 확실한 구현과 개발자간 의사 소통을 위해선 set에 담는게 좋지만 for for (i,j) 루프를 돌기 때문에 list에 담아도 중복될 일은 존재하지 않기 때문에 list 를 사용했습니다. 또한 시간 초를 조금이라도 줄이자는 목표가 있었습니다.
    
    3. 마지막은 그룹 생성입니다. 그룹은 인접해서 연결된 cnt를 찾아면 됐습니다. 결국 탐색을 해야하는데 dfs를 사용했습니다. 사실 bfs를 사용해도 되지만, 개인 선호도로 인해 dfs를 사용했습니다.
    
    <성찰>
    1. 함수를 나누는데 익숙해졌음.
    2. 90도 회전할 수 있는 더 좋은 함수가 있는지 찾아봐야겠음. 현재 방법은 너무 연산 소모가 큼. reverse를 안하면 2배로 줄일 수 있을 것으로 예상됨.

--- Python/BaekJun/Baek20058.py
+++ Python/BaekJun/Baek20058.py
@@ -20,7 +20,7 @@ def getCW2DArray(array):
     return getLRReverseArray(get2DTranspose(array))
 
 def rotatePartial2DArray(graph, si, sj, L):
-    temp2DArray = [[graph[i][j] for j in range(sj, sj + 2**L)] for i in range(si, si + 2**L)]
+    temp2DArray = [graph[i][sj:sj+2**L] for i in range(si, si+2**L)]
     cw2DArray = getCW2DArray(temp2DArray)
 
     # graph에 적용
@@ -30,7 +30,8 @@ def rotatePartial2DArray(graph, si, sj, L):
 
 def rotateWith2LRule(graph, L):
     global N
-
+    if L == 0:
+        return
     # 부분 2차원 배열 시작 인덱스 구하기
     startIdx = []
     for i in range(0, 2**N, 2**L):
@@ -40,12 +41,6 @@ def rotateWith2LRule(graph, L):
     # 부분 2차원 배열 시작 인덱스와 L로 각 부분 2차원 배열 회전하기
     for i, j in startIdx:
         rotatePartial2DArray(graph, i, j, L)
-
-def print2Darray(message, array):
-    print(message)
-    for line in array:
-        print(line)
-    print("============================")
 
 def getAdjacentIceCnt(graph, i, j):
     global N, di, dj
@@ -65,11 +60,17 @@ def getAdjacentIceCnt(graph, i, j):
 
 def startMelting(graph):
     global N
+    meltNeedList = []
     for i in range(2**N):
         for j in range(2**N):
+            if graph[i][j] <= 0:
+                continue
             adjacentIceCnt = getAdjacentIceCnt(graph, i, j)
-            if adjacentIceCnt <= 2:
-                graph[i][j] -= 1
+            if not (adjacentIceCnt >= 3):
+                meltNeedList.append((i, j))
+
+    for i, j in meltNeedList:
+        graph[i][j] -= 1
 
 def doFireStorm(graph, L):
     rotateWith2LRule(graph, L)
@@ -84,7 +85,41 @@ def getIceLefts(graph):
 
     return total
 
+def getBiggestGroupSizeByDfs(graph):
+    global N, di, dj
+    visited = set([])
+    maxSize = 0
+    for i in range(2**N):
+        for j in range(2**N):
+            start = (i, j)
+            if graph[i][j] == 0:
+                continue
+            if start in visited:
+                continue
+            stack = [start]
+            visited.add(start)
+            size = 0
+            while stack:
+                ci, cj = stack.pop()
+                size += 1
+                maxSize = max(size, maxSize)
+
+                for idx in range(4):
+                    ni = ci + di[idx]
+                    nj = cj + dj[idx]
+                    if not(0 <= ni < 2**N and 0 <= nj < 2**N):
+                        continue
+                    if graph[ni][nj] == 0:
+                        continue
+                    if (ni, nj) in visited:
+                        continue
+                    visited.add((ni, nj))
+                    stack.append((ni, nj))
+    return maxSize
+
+
 for L in LevelList:
     doFireStorm(graph, L)
 
 print(getIceLefts(graph))
+print(getBiggestGroupSizeByDfs(graph))
