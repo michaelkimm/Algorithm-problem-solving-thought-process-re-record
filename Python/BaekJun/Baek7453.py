5fcfadd68e7cbcde5c2e80303fa8995ed0054e0e
michael <mskim_451@dongguk.edu>
2022-08-20 10:58:39 +0900

    백준.7453.풀이완료.재풀이
    알고리즘 : 이분 탐색, 구현
    자료 구조 : dict
    사고 과정
    1. 합이 0이 되는 네개의 정수 쌍을 구하면 된다. 간단하게는 브루트포스로 구할 수 있다. 네 정수의 조합으로 구하면 된다. 4000 ^ 4 /(4!)이 된다. 하지만 시간 제한을 한참 초과하기 때문에 해당 방식은 배제했다. 경우의 수가 너무 많으면 미리 구하는 방법이 있다. A와 B의 합과 C와 D의 합을 모두 구한 후 두 요소의 합이 0이 되는 경우를 찾으면 된다. 이때는 두가지 방법을 생각할 수 있다. dict를 이용하여 갯수까지 한번에 계산하는 법, 이분 탐색으로 일일히 탐색하는법이 있다.
    
    2. dict를 이용하는 방법은 dict를 만드는데 O(N^2)이 걸리고 dict에서 정답을 찾는데 O(1)이 걸리기 때문에 시간 제한을 통과하지만 결과는 시간 초과였다. python으로 통과한 사람이 1명뿐인걸 보면, python 자체가 느려서인듯하다. 이분 탐색 또한 같은 이유로 실패했다.
    
    3. 이후 반복되는 for문을 최대한 줄이고 함수 내에 있는 코드도 전부 밖으로 꺼냈다. 함수를 찾으러가는 연산 마저 줄여봤다. 이후 pypy3으로 돌려보니 통과했다.
    
    <성찰>
    1. 시간 복잡도를 충분히 만족하고 무한 루프를 돌리고 있는 것도 아닌데 시간 초과가 난다면 언어 자체의 문제일 수도 있다. 이러한 극한의 시간 제한 문제는 알고리즘 대회에서나 나오지 일반적인 기업 코딩 테스트에선 나오지 않는다. 따라서 이럴 땐 과감히 사람들의 후기를 보자.

--- Python/BaekJun/Baek7453.py
+++ Python/BaekJun/Baek7453.py
@@ -2,62 +2,6 @@ from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-def getCartasianProductSum(ary1, ary2):
-    ret = []
-    for v1 in ary1:
-        for v2 in ary2:
-            ret.append(v1 + v2)
-    ret.sort()
-    return ret
-
-def bisect_left(ary, target):
-    left = 0
-    right = len(ary) - 1
-    ret = -1
-    while left <= right:
-        mid = (left + right) // 2
-        if ary[mid] == target:
-            ret = mid
-            break
-        elif ary[mid] > target:
-            right = mid - 1
-        elif ary[mid] < target:
-            left = mid + 1
-    if ret != -1:
-        while ret >= 0 and ary[ret] == target:
-            ret -= 1
-        ret += 1
-    return ret
-
-def bisect_right(ary, target):
-    left = 0
-    right = len(ary) - 1
-    ret = -1
-    while left <= right:
-        mid = (left + right) // 2
-        if ary[mid] == target:
-            ret = mid
-            break
-        elif ary[mid] > target:
-            right = mid - 1
-        elif ary[mid] < target:
-            left = mid + 1
-    if ret != -1:
-        while ret < len(ary) and ary[ret] == target:
-            ret += 1
-        ret -= 1
-    return ret
-
-def getCasesCntWhichMakesSumZero(cases, num):
-    
-    left = bisect_left(cases, -num)
-    right = bisect_right(cases, -num)
-    
-    if left == -1:
-        return 0
-    else:
-        return right - left + 1
-
 n = int(input())
 As = []
 Bs = []
@@ -70,12 +14,16 @@ for _ in range(n):
     Cs.append(c)
     Ds.append(d)
 
-abSums = getCartasianProductSum(As, Bs)
-cdSums = getCartasianProductSum(Cs, Ds)
-
+abSumsCnt = defaultdict(int)
 answer = 0
+for v1 in As:
+    for v2 in Bs:
+        abSumsCnt[v1 + v2] += 1
 
-for abSum in abSums:
-    answer += getCasesCntWhichMakesSumZero(cdSums, abSum)
+for v1 in Cs:
+    for v2 in Ds:
+        sumVal = v1 + v2
+        if -sumVal in abSumsCnt:
+            answer += abSumsCnt[-sumVal]
 
 print(answer)
