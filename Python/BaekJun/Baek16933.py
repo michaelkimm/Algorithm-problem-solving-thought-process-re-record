a98ee279a5baf39557cba161ad5bf408e256d77a
michael <mskim_451@dongguk.edu>
2022-09-17 16:43:41 +0900

    백준.16933.풀이완료
    -

--- Python/BaekJun/Baek16933.py
+++ Python/BaekJun/Baek16933.py
@@ -3,8 +3,8 @@ import sys
 input = sys.stdin.readline
 
 # 동,서,남,북,제자리
-di = [0, 0, 1, -1, 0]
-dj = [1, -1, 0, 0, 0]
+di = [0, 0, 1, -1]
+dj = [1, -1, 0, 0]
 
 N, M, K = map(int, input().split())
 graph = [list(map(int, input().strip())) for _ in range(N)]
@@ -23,11 +23,19 @@ def bfs(graph, K):
     ret = -1
     while q:
         ci, cj, moveCnt, K, state = q.popleft()
+        newState = 1 if state == 0 else 0
+
         if ci == N - 1 and cj == M - 1:
             ret = moveCnt
             break
 
-        for dirIdx in range(5):
+        # 제자리
+        if not visited[ci][cj][K][newState]:
+            q.append((ci, cj, moveCnt + 1, K, newState))
+            visited[ci][cj][K][newState] = True
+
+
+        for dirIdx in range(4):
             ni, nj = ci + di[dirIdx], cj + dj[dirIdx]
             if not (0 <= ni < N and 0 <= nj < M):
                 continue
@@ -35,19 +43,27 @@ def bfs(graph, K):
             newK = K
             
             if graph[ni][nj] == 1:
-                if newK == 0:
+                if state == 1:
+                    # 낮인 경우
+                    if newK == 0:
+                        continue
+                    else:
+                        newK -= 1
+                else:
+                    # 밤인 경우
                     continue
-                elif state == 1 and newK >= 1:
-                    newK -= 1
-                elif state == 0:
-                    continue
-
-            newState = 1 if state == 0 else 0
-
+                        
             if visited[ni][nj][newK][newState]:
                 continue
             q.append((ni, nj, moveCnt + 1, newK, newState))
             visited[ni][nj][newK][newState] = True
+
+            # if graph[ni][nj] == 0 and not visited[ni][nj][K][newState]:            
+            #     q.append((ni, nj, moveCnt + 1, K, newState))
+            #     visited[ni][nj][K][newState] = True
+            # elif graph[ni][nj] == 1 and state == 1 and K > 1 and not visited[ni][nj][K - 1][newState]:
+            #     q.append((ni, nj, moveCnt + 1, K - 1, newState))
+            #     visited[ni][nj][K - 1][newState] = True
                     
     return ret
 
