d76d500b0b4d1f50038a7183d11b93f3f1e99b62
michael <mskim_451@dongguk.edu>
2022-10-10 19:34:16 +0900

    백준.2559.풀이완료
    알고리즘 : dp
    자료구조 : 배열
    사고 과정
    - 배열 내 연속된 K 개 수의 합중 가장 큰 것을 구해야함. 브루트포스는 O(KN)이지만 누적합은 O(N)이기 때문에 누적합으로 해결

--- Java/BaekJoon/2559.java
+++ Java/BaekJoon/2559.java
@@ -1,21 +1,51 @@
-class Main {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine(" ");
-        N = Integer.parseInt(input[0]);
-        K = Integer.parseInt(input[1]);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-        input = br.readLine(" ");
-        int[] temperatures = int[N];
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        input = br.readLine().split(" ");
+        int[] temperatures = new int[N];
         for (int i = 0; i < N; i++) {
-            culmulativeSum[i] = Integer.parseInt(pinput[i]);
+            temperatures[i] = Integer.parseInt(input[i]);
         }
 
         int[] culmulativeSum = getCumulativeSum(temperatures);
-        
+
+        int left = -1;
+        int right = K - 1;
+        int maxTemperatureSum = Integer.MIN_VALUE;
+        while (right < N) {
+            int sum = 0;
+            if (left >= 0) {
+                sum = culmulativeSum[right] - culmulativeSum[left];
+            } else {
+                sum = culmulativeSum[right];
+            }
+            maxTemperatureSum = sum > maxTemperatureSum ? sum : maxTemperatureSum;
+            left += 1;
+            right += 1;
+        }
+
+        System.out.println(maxTemperatureSum);
     }
 
     public static int[] getCumulativeSum(int[] ary) {
-
+        int[] dp = new int[ary.length];
+        for (int i = 0; i < ary.length; i++) {
+            if (i == 0) {
+                dp[i] = ary[0];
+                continue;
+            }
+            dp[i] = dp[i - 1] + ary[i];
+        }
+        return dp;
     }
 }
