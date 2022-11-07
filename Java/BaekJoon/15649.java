aac700342d0e6172f6ddc1b02e4359b1dee94762
michael <mskim_451@dongguk.edu>
2022-11-07 20:01:42 +0900

    백준.15649,15650,15651,15652.풀이완료
    알고리즘 : 백트랙킹, dfs
    자료구조 : -
    사고 과정
    1. 백트랙킹은 dfs로 표현될 수 있다. 즉 백트랙킹 내부에서도 노드, 간선이 존재한다는 뜻이다. 노드는 현재 까지 어떤 데이터를 선택했었나의 기록이고, 간선은 그 다음 데이터를 어떤 것으로 선택할 것인가 이다.
    2. 백트랙킹을 사용할 때 매 노드마다 새로 노드를 생성하면 원활히 매 노드의 데이터 독립성이 보장되지만, 그렇게 하면 메모리 소모가 크다. 따라서 모든 노드가 전역 변수 하나를 참조하도록 하면, 이를 해결할 수 있다. 하지만 이때 데이터 무결성 문제가 생기기 때문이 이를 신경써줘야한다.

---
+++ Java/BaekJoon/15649.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+class Main {
+
+    static int N;
+    static int M;
+
+    static boolean[] visited;
+
+    static LinkedList<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.valueOf(input[0]);
+        M = Integer.valueOf(input[1]);
+
+        list = new LinkedList<>();
+        visited = new boolean[N + 1];
+
+        getPermutation(0);
+    }
+
+    static void getPermutation(int pickedCnt) {
+        if (pickedCnt == M) {
+            for (Integer integer : list) {
+                System.out.print(integer.toString() + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            list.add(i);
+            getPermutation(pickedCnt + 1);
+            visited[i] = false;
+            list.pollLast();
+        }
+        return;
+    }
+}
---
+++ Java/BaekJoon/15652.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+class Main {
+
+    static int N;
+    static int M;
+
+    static LinkedList<Integer> list;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.valueOf(input[0]);
+        M = Integer.valueOf(input[1]);
+
+        list = new LinkedList<>();
+
+        getPermutation(1, 0);
+        System.out.println(sb.toString());
+    }
+
+    static void getPermutation(int before, int pickedCnt) {
+        if (pickedCnt == M) {
+            for (Integer integer : list) {
+                sb.append(integer.toString() + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = before; i <= N; i++) {
+            list.add(i);
+            getPermutation(i, pickedCnt + 1);
+            list.pollLast();
+        }
+        return;
+    }
+}
