800a8b44e71be51abef543b1b816815a9dc500af
michael <mskim_451@dongguk.edu>
2022-11-23 12:27:55 +0900

    백준.1339.풀이완료
    알고리즘 : 그리디
    자료구조 : -
    사고 과정
    1. 총합을 최고로 크게 만드는 알파벳-숫자 매칭 방법을 찾으면 된다. 총합을 최고로 크게 만들려면 어떤 알파벳에게 높은 숫자를 줘야할까? 우선 자리 수가 큰 알파벳에게 높은 숫자를 주는 그리디한 방법을 생각할 수 있다. 그리디 접근법의 정당성을 증명해보자. 자리수가 더 작은 알파벳에게 더 큰 숫자를 준다고 생각해보자. 이때 아래와 같이 단어들이 존재하게 되면, 그리디한 접근 방법으론 A에 9를 주고 C에 8을 줘야한다. 우린 정당성을 증명 중이니 A에게 8을 주고 C에게 9를 주자. 이때 후자가 더 큰 결과 값이 나온다. 즉 자리수 만으로 숫자를 부여하면 안된다.
    ACC
    CC
    CC
    CC
    CC
    CC
    CC
    CC
    CC
    CC
    
    2. 높은 자리 숫자에 있는 알파벳들의 합 보다 낮은 자리 숫자에 있는 알파벳들의 합이 더 높으면 낮은 자리 숫자에 있는 알파벳에 높은 숫자를 부여하는게 좋다. 따라서 알파벳의 자리수 가중치로 생각하여 해당 알파벳의 힘을 계산하자. 계산한 알파벳 중 가장 힘이 쎈 알파벳에게 높은 점수를 주면 된다.
    
    <Review>
    1. 이번에도 그리디의 정당성 검증을 꼼꼼하게 하지 못했다.
    2. 그리디의 정당성 검증을 식으로서 표현하도록 노력해보자.

--- Python/BaekJun/Baek1339.py
+++ Python/BaekJun/Baek1339.py
@@ -2,58 +2,27 @@ from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-class AlphabetInfo:
-    def __init__(self, alphabet, highestDigit):
-        self.alphabet = alphabet
-        self.highestDigit = highestDigit
-        self.count = 1
-
-    def __str__(self):
-        return "alphabet:" + str(self.alphabet) + "\thighestDigit:" + str(self.highestDigit) + "\tcount:" + str(self.count)
-
-    def checkHighestDigit(self, newDigit):
-        if self.highestDigit < newDigit:
-            self.highestDigit = newDigit
-
-    def increaseCount(self):
-        self.count += 1
 
 N = int(input())
+weight = defaultdict(int)
 words = []
 for i in range(N):
     words.append(input().strip())
-    words[i] = words[i][::-1]
+    for idx in range(len(words[i])):
+        weight[words[i][idx]] += 10**(len(words[i]) - idx - 1)
 
-alphabetInfoDict = dict()
+dicts = dict()
+weightList = sorted(weight.items(), key=lambda x:x[1], reverse=True)
+value = 9
+for ch, w in weightList:
+    dicts[ch] = value
+    value -= 1
 
-for i in range(N):
-    for idx, ch in enumerate(words[i]):
-        if ch in alphabetInfoDict.keys():
-            alphabetInfoDict[ch].checkHighestDigit(idx)
-            alphabetInfoDict[ch].increaseCount()
-        else:
-            alphabetInfoDict[ch] = AlphabetInfo(ch, idx)
+sum = 0
+for word in words:
+    tmpNum = ''
+    for ch in word:
+        tmpNum += str(dicts[ch])
+    sum += int(tmpNum)
 
-alphabetInfoList = []
-for key, item in alphabetInfoDict.items():
-    alphabetInfoList.append(item)
-
-alphabetInfoList.sort(key=lambda x:(x.highestDigit, x.count), reverse=True)
-
-for info in alphabetInfoList:
-    print(info)
-
-alphabetWeightDict = defaultdict(int)
-for idx, alphabetInfo in enumerate(alphabetInfoList):
-    ch = alphabetInfo.alphabet
-    alphabetWeightDict[ch] += (9 - idx)
-
-
-for idx in range(len(words)):
-    for key in alphabetWeightDict.keys():
-        words[idx] = words[idx].replace(key, str(alphabetWeightDict[key]))
-    words[idx] = words[idx][::-1]
-
-print(words)
-sum = sum([int(val) for val in words])
 print(sum)
