e8cb9ff8a86c1ef271c85e22c05a86627fd4da96
michael <mskim_451@dongguk.edu>
2023-04-02 14:21:17 +0900

    백준.16112.풀이완료
    복기 필요 x

---
+++ Python/BaekJun/Baek16112.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+stoneValues = sorted(list(map(int, input().split())))
+
+answer = 0
+
+activatedCnt = 0
+for i in range(n):
+    # if not activatedStoneDict.keys():
+    #     activatedStoneDict[i] = stoneValues[i]
+    if activatedCnt < k:
+        # 기존에 활성화된 돌에 경험치 추가
+        for j in range(activatedCnt):
+            answer += stoneValues[i]
+        activatedCnt += 1
+    else:
+        # 기존에 활성화된 돌에 경험치 추가
+        for j in range(activatedCnt):
+            answer += stoneValues[i]
+
+
+print(answer)
