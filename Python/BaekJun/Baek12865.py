2a47dccb977f6db76c4b0cbc0ca621c912fd09c5
michael <mskim_451@dongguk.edu>
2023-06-16 11:57:08 +0900

    백준.7579,12865.풀이완료
    알고리즘: dp, 냅색
    자료구조: 배열
    잘한 점(Keep)
    1. 규칙을 찾고, 문제를 명확히 정의하고 수도코드까지 차분히 짠 점
    2. 코드로 옮긴 후 테스트 케이스를 한번에 통과한 점
    
    아쉬운 점(Problem)
    1. 예외 케이스를 꼼꼼히 보지 않은 점. 특정 예외 케이스를 예상했지만, 될 것으로 추상적으로 판단하기만함.
    2. 제한 조건을 제대로 보지 않아서 시간 복잡도 계산을 잘 못한 것
    
    앞으로 할 것(Try)
    1. 앞으론 예외 케이스를 꼭 손&펜으로 검증해보기
    2. 문제 조건 꼼꼼히 읽기

--- Python/BaekJun/Baek12865.py
+++ Python/BaekJun/Baek12865.py
@@ -2,18 +2,12 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-items = [list(map(int, input().split())) for _ in range(N)]
+items = sorted([list(map(int, input().split())) for _ in range(N)])
 
-dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+dp = [0 for _ in range(K + 1)]
 
-for i in range(1, N):
-    w = items[i][0]
-    v = items[i][1]
-    for j in range(1, K + 1):
+for weight, value in items:
+    for i in range(K, weight - 1, -1):
+        dp[i] = max(dp[i - weight] + value, dp[i])
 
-        if w > j:
-            dp[i][j] = dp[i - 1][j]
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
-
-print(max(list(max(v) for v in dp)))
+print(dp[K])
--- Python/BaekJun/Baek7579.py
+++ Python/BaekJun/Baek7579.py
@@ -1,24 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-INF = int(1e10)
-
 N, M = map(int, input().split())
-memorys = [0] + list(map(int, input().split()))
-costs = [0] + list(map(int, input().split()))
-costSum = sum(costs)
+memorys = list(map(int, input().split()))
+costs = list(map(int, input().split()))
 
-dp = [[0 for _ in range(costSum + 1)] for _ in range(N + 1)] # i번째까지 앱을 사용하여 j 비용을 사용했을 때얻을 수 있는 최대 메모리 바이트 수
+dp = [0 for _ in range(10001)]
+dp[0] = 0
 
-for i in range(1, N + 1):
-    for j in range(costSum + 1):
-        dp[i][j] = dp[i - 1][j]
-        if j - costs[i] >= 0:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j - costs[i]] + memorys[i])
+for i in range(N):
+    mem = memorys[i]
+    cost = costs[i]
+    for j in range(10000, -1, -1):
+        dp[j] = max(dp[j - cost] + mem, dp[j]) if j >= cost else dp[j]
 
-answer = -1
-for j in range(costSum + 1):
-    if dp[N][j] >= M:
-        answer = j
+answer = 0
+for i in range(10001):
+    if dp[i] >= M:
+        answer = i
         break
 print(answer)
